8e5e675a292aab380ee105dca8f98dba48fb4736
Dan Grebb <dan@dgrebb.com>
2023-05-12 20:09:33 -0400

    feat(strapi): installs and configures strapi s3 provider

---
+++ strapi/config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: `//${env("CDN_BASE_URL")}`,
+        rootPath: "img",
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 60 * 60 * 24 * 7),
+            Bucket: env("AWS_S3_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
