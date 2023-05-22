5bc1f0846e1727c15ec18197800e3ef380f02775
Dan Grebb <dan@dgrebb.com>
2023-05-22 16:28:21 -0400

    feat(front): adds domains for images config

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["localhost", "127.0.0.1", "local.s.dgrebb.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
