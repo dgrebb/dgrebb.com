e4fe7b4251ce139ab533a5a2fddd669ed5774d47
Dan Grebb <dan@dgrebb.com>
2023-05-23 21:22:12 -0400

    feat(utils): adds changelog, utils, and comments for setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 directoryx="$(dirname -- $(readlink -fn -- "$0"; echo x))"
 directory="${directoryx%x}"
 sudo ln -fs ${directory}/_utils/dg /usr/local/bin
+
+#TODO: Install Changelog util
+    # install cargo
+    # `cargo install git-cliff`
