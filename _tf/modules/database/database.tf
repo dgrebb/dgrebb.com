f1882cb28c1156a752c4032883c6b515d7f64b01
Dan Grebb <dan@dgrebb.com>
2023-06-01 22:16:26 -0400

    refactor(terraform): updates database and ecs configurations to test cost savings
    reduced the cpu and memory on ecs tasks, and also reduced the rds instance to db.t2.micro and set it by variable so prod and stg can have different configs

--- _tf/modules/database/database.tf
+++ _tf/modules/database/database.tf
@@ -2,19 +2,23 @@
 # CMS Database
 # ------------------------------------------------------------------------------
 resource "aws_db_instance" "this" {
-  depends_on             = [var.service_sg]
-  identifier             = var.dashed_cmsdomain
-  allocated_storage      = 10
-  engine                 = "postgres"
-  engine_version         = "10.23"
-  instance_class         = "db.t2.small"
-  db_name                = "strapi"
-  username               = "strapi"
-  password               = var.db_password
-  vpc_security_group_ids = ["${var.service_sg.id}"]
-  db_subnet_group_name   = var.db_subnet_group.id
-  skip_final_snapshot    = "true"
+  depends_on                = [var.service_sg]
+  identifier                = var.dashed_cmsdomain
+  allocated_storage         = 10
+  engine                    = "postgres"
+  engine_version            = "10.23"
+  instance_class            = var.instance_class
+  db_name                   = "strapi"
+  username                  = "strapi"
+  password                  = var.db_password
+  vpc_security_group_ids    = ["${var.service_sg.id}"]
+  db_subnet_group_name      = var.db_subnet_group.id
+  skip_final_snapshot       = var.skip_final_snapshot
+  final_snapshot_identifier = var.dashed_cmsdomain
+  apply_immediately         = true
+  backup_retention_period   = 3
+  backup_window             = "06:00-07:00"
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
--- _tf/modules/database/inputs.tf
+++ _tf/modules/database/inputs.tf
@@ -1,6 +1,8 @@
 variable "dashed_cmsdomain" {}
 variable "service_sg" {}
 variable "db_subnet_group" {}
+variable "instance_class" {}
+variable "skip_final_snapshot" {}
 variable "db_password" {
   sensitive = true
 }
