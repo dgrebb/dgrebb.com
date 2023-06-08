b50ee0e34a0b09bca746d8d86de37c84b86302cb
Dan Grebb <dan@dgrebb.com>
2023-06-08 06:03:59 -0400

    feat(front): adds svelte ssg adapter and configuration

--- front/src/routes/+layout.server.js
+++ front/src/routes/+layout.server.js
@@ -2,6 +2,8 @@ import { error } from "@sveltejs/kit";
 import api from "../api";
 import { PUBLIC_API_URL as endpoint } from "$env/static/public";
 
+export const prerender = true;
+
 const navigationEndpoint = `${endpoint}/navigation?populate=*`;
 const footerEndpoint = `${endpoint}/footer?populate=*`;
 
