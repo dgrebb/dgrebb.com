9b8c3aa66c20edd84a640f0f1cf9c02215e193fc
Dan Grebb <dan@dgrebb.com>
2023-06-10 14:22:51 -0400

    fix(front): removes svelte fallback 404 config

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -1,18 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		appDir: "s",
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: true,
-			strict: true
-	})
-	},
+  kit: {
+    appDir: "s",
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      precompress: true,
+      strict: true,
+    }),
+  },
   preprocess: vitePreprocess(),
 };
 
