218a01748c89ae24c58a5a360028cf1fc74c3d70
Dan Grebb <dan@dgrebb.com>
2023-06-10 16:44:39 -0400

    style(front): adjusts meta description text

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
   import { MetaTags } from "svelte-meta-tags";
   import Image from "../components/Image.svelte";
-  import Loading from "../components/Loading.svelte";
   import Links from "../components/Links.svelte";
   import SvelteMarkdown from 'svelte-markdown'
   import Head from "../components/Head.svelte";
   export let data;
   const { headshot, headline, intro, links } = data;
   const image = headshot?.data?.attributes.formats.thumbnail || false;
+  const alt = headshot?.data?.attributes.alternativeText;
 </script>
 
 <section class="bio text-center">
@@ -31,35 +31,30 @@
 {/if}
 
 <Head>
-  <meta name="thumbnail" content="https://stg.s.dgrebb.com/img/bio_164dda2bdc.jpg" />
+  <meta
+    name="thumbnail"
+    content="https://stg.s.dgrebb.com/img/bio_164dda2bdc.jpg"
+  />
 </Head>
 
 <MetaTags
   title="Dan Grebb"
-
-  description="Dan Grebb is a versatile and accomplished
-  individual with a passion for innovation and creative problem-solving. With a
-  strong background in technology and a knack for strategic thinking, Dan
-  consistently delivers exceptional results. Whether leading teams, driving
-  business growth, or spearheading complex projects, Dan's expertise shines
-  through. With an impressive track record of success, Dan Grebb is an
-  invaluable asset to any organization seeking a dynamic and forward-thinking
-  professional."
-
+  description="Dan Grebb is a versatile and accomplished individual with a passion for innovation and creative problem-solving. Dan is an invaluable asset to any organization seeking a dynamic and forward-thinking professional."
   openGraph={{
     images: [
       {
-        url: 'https://stg.s.dgrebb.com/img/default_banner_8464dc0d50.png',
+        url: "https://stg.s.dgrebb.com/img/default_banner_8464dc0d50.png",
         width: 1600,
         height: 900,
-        alt: 'The Circuit of Life'
+        alt: "The Circuit of Life",
       },
       {
-        url: 'https://stg.s.dgrebb.com/img/circuit_of_life_800x600_f0b453b8f9.png',
+        url:
+          "https://stg.s.dgrebb.com/img/circuit_of_life_800x600_f0b453b8f9.png",
         width: 800,
         height: 600,
-        alt: 'The Circuit of Life'
-      }
-    ]
+        alt: "The Circuit of Life",
+      },
+    ],
   }}
 />
