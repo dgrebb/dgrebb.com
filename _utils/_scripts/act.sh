5661c85272bb3068062ad779bfa3ae24d4eaa67f
Dan Grebb <dan@dgrebb.com>
2023-06-12 19:43:22 -0400

    fix(project): updates act utils with options for job selection

--- _utils/_scripts/act.sh
+++ _utils/_scripts/act.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 source $directory/_scripts/functions.sh
 
+if [ $2 == "f" ]; then
+    action="front"
+elif [ $2 == "b" ]; then
+    action="back"
+elif [ $2 == "d" ]; then
+    action="dispatch"
+else
+    printDgErr "Argument needed to specify job."
+fi
+
 act \
--s GITHUB_TOKEN=$(pass dg/github/pat) \
--s DEPLOYMENT_BRANCH=develop \
--s PUBLIC_ENV=staging \
--s PUBLIC_API_URL=$(pass dg/api/${1}/url) \
--s API_KEY=$(pass dg/api/${1}/apikey) \
--s PUBLIC_SENTRY_DSN=$(pass dg/www/logs/sentrydsn) \
--s AWS_ACCESS_KEY_ID=$(pass dg/aws/id) \
--s AWS_SECRET_ACCESS_KEY=$(pass dg/aws/secret) \
--s AWS_REGION=$(pass dg/aws/region) \
--s AWS_S3_BUCKET=$(pass dg/www/${1}/bucket) \
--s DISTRIBUTION=$(pass dg/aws/${1}/distribution) \
---container-architecture linux/amd64 \
-repository_dispatch
+    -s GITHUB_TOKEN=$(pass dg/github/pat) \
+    -s DEPLOYMENT_BRANCH=develop \
+    -s PUBLIC_ENV=staging \
+    -s PUBLIC_API_URL=$(pass dg/api/${1}/url) \
+    -s API_KEY=$(pass dg/api/${1}/apikey) \
+    -s PUBLIC_SENTRY_DSN=$(pass dg/www/logs/sentrydsn) \
+    -s AWS_ACCESS_KEY_ID=$(pass dg/aws/id) \
+    -s AWS_SECRET_ACCESS_KEY=$(pass dg/aws/secret) \
+    -s AWS_REGION=$(pass dg/aws/region) \
+    -s AWS_S3_BUCKET=$(pass dg/www/${1}/bucket) \
+    -s DISTRIBUTION=$(pass dg/aws/${1}/distribution) \
+    --container-architecture linux/amd64 \
+    -j $action
