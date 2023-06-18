3c9bfbb8b39725c46e260d82549e33745867b678
Dan Grebb <dan@dgrebb.com>
2023-06-17 20:23:12 -0400

    feat(front): uses seo data from cms; adds media url for dev env

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script>
+  import { PUBLIC_MEDIA_URL } from "$env/static/public";
   import { MetaTags } from "svelte-meta-tags";
   import Image from "../components/Image.svelte";
   import Links from "../components/Links.svelte";
   import SvelteMarkdown from 'svelte-markdown'
   import Head from "../components/Head.svelte";
   export let data;
-  const { headshot, headline, intro, links } = data;
-  const image = headshot?.data?.attributes.formats.thumbnail || { url: "/bio.png", alt: "A picture of Dan" };
-  const alt = headshot?.data?.attributes.alternativeText;
+  const { seo, headline, intro, links } = data;
+  const image = seo?.metaImage?.data?.attributes.formats.small || { url: "/bio.png", alt: "A picture of Dan smiling" };
+  const metaSocialOG = seo?.metaSocial.find((obj) => obj.socialNetwork === "Facebook");
 </script>
 
 <section class="bio text-center">
   {#if image}
     <Image
-      src={image.url}
+      src={`${PUBLIC_MEDIA_URL}${image.url}`}
       alt={image.alternativeText}
       title="Hi!"
       width={120}
@@ -31,29 +32,26 @@
 {/if}
 
 <Head>
-  <meta
-    name="thumbnail"
-    content="https://stg.s.dgrebb.com/img/bio_164dda2bdc.jpg"
-  />
+  <meta name="viewport" content={seo.metaViewport} />
+  <meta name="thumbnail" content={`${PUBLIC_MEDIA_URL}${image.url}`} />
 </Head>
 
 <MetaTags
-  title="Dan Grebb"
-  description="Dan Grebb is a versatile and accomplished individual with a passion for innovation and creative problem-solving. Dan is an invaluable asset to any organization seeking a dynamic and forward-thinking professional."
+  title={seo.metaTitle}
+  description={seo.metaDescription}
   openGraph={{
     images: [
       {
-        url: "https://stg.s.dgrebb.com/img/default_banner_8464dc0d50.png",
+        url: `${PUBLIC_MEDIA_URL}${metaSocialOG.image.data.attributes.url}`,
         width: 1600,
         height: 900,
-        alt: "The Circuit of Life",
+        alt: metaSocialOG.image.data.attributes.alternativeText,
       },
       {
-        url:
-          "https://stg.s.dgrebb.com/img/circuit_of_life_800x600_f0b453b8f9.png",
+        url: `${PUBLIC_MEDIA_URL}${metaSocialOG.image.data.attributes.formats.medium.url}`,
         width: 800,
         height: 600,
-        alt: "The Circuit of Life",
+        alt: metaSocialOG.image.data.attributes.alternativeText,
       },
     ],
   }}
