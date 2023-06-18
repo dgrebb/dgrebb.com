299dfc475c652c4ba580cd69899e7dd1637b798e
Dan Grebb <dan@dgrebb.com>
2023-06-17 20:52:08 -0400

    ci(front): uncomments build script

--- _ci/build.sh
+++ _ci/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 printf "\033[0;33mInstalling Node packages...\033[0m\n"
-# cd front
-# npm ci
+cd front
+npm ci
 printf "\033[0;32mBuilding...\033[0m\n"
-# echo "Building the Frontend..."
-# npm run build
+echo "Building the Frontend..."
+npm run build
