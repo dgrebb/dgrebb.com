3836e5dea408630844617a07e4317ab6fec76bf8
Dan Grebb <dan@dgrebb.com>
2023-06-17 22:20:50 -0400

    build(front): throws error if nav or footer apis fail

--- front/src/routes/+layout.server.js
+++ front/src/routes/+layout.server.js
@@ -14,6 +14,11 @@ const footerEndpoint = `${PUBLIC_API_URL}${PUBLIC_API_PATH_FOOTER}`;
 export async function load({ params }) {
   const navigationContent = await api(navigationEndpoint);
   const footerContent = await api(footerEndpoint);
+
+  if (!navigationContent || !footerContent) {
+    throw error(404, "/404");
+  }
+
   return {
     ...navigationContent.attributes,
     ...footerContent.attributes,
