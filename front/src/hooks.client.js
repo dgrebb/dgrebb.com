a56749da5ad8a74bcdb98af7a1c1d0dc01b418ae
Dan Grebb <dan@dgrebb.com>
2023-06-22 01:50:37 -0400

    fix(front): removes crypto and uuid from client logging

--- front/src/hooks.client.js
+++ front/src/hooks.client.js
@@ -1,5 +1,6 @@
 import * as Sentry from "@sentry/sveltekit";
 import { PUBLIC_ENV, PUBLIC_SENTRY_DSN } from "$env/static/public";
+import crypto from "node:crypto";
 
 Sentry.init({
   dsn: PUBLIC_SENTRY_DSN,
@@ -11,9 +12,9 @@ Sentry.init({
     }),
     new Sentry.Replay(),
   ],
-  tracesSampleRate: ( PUBLIC_ENV === "production" ? 0.1 : 1.0 ),
-  replaysSessionSampleRate: ( PUBLIC_ENV === "production" ? 0.1 : 1.0 ),
-  replaysOnErrorSampleRate: ( PUBLIC_ENV === "production" ? 0.1 : 1.0 ),
+  tracesSampleRate: PUBLIC_ENV === "production" ? 0.1 : 1.0,
+  replaysSessionSampleRate: PUBLIC_ENV === "production" ? 0.1 : 1.0,
+  replaysOnErrorSampleRate: PUBLIC_ENV === "production" ? 0.1 : 1.0,
   beforeSend(event) {
     if (event.user) {
       delete event.user.ip;
@@ -21,16 +22,14 @@ Sentry.init({
     if (event.server_name) {
       delete event.server_name;
     }
-  }
+  },
 });
 
 export async function handleError({ error, event }) {
-  const errorId = crypto.randomUUID();
   Sentry.setTag("environment", PUBLIC_ENV);
-  Sentry.captureException(error, { extra: { event, errorId } });
+  Sentry.captureException(error, { extra: { event } });
 
   return {
     message: "Client error.",
-    errorId,
   };
 }
