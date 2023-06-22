74b37a71e4ec5e9e634276c5397dce67615cb469
Dan Grebb <dan@dgrebb.com>
2023-06-22 00:07:16 -0400

    feat(front): adds Code component as a markdown renderer

--- front/src/routes/privacy/+page.svelte
+++ front/src/routes/privacy/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import SvelteMarkdown from 'svelte-markdown';
   import Flourish from '../../components/Flourish.svelte';
+  import Code from "../../components/markdown/Code.svelte";
   import Link from "../../components/markdown/Link.svelte";
 
   export let data;
@@ -10,5 +11,5 @@
 <section class="privacy">
   <Flourish />
   <h1 class="title">{title}</h1>
-  <SvelteMarkdown renderers={{ link: Link }} source={details} />
+  <SvelteMarkdown renderers={{ link: Link, code: Code }} source={details} />
 </section>
