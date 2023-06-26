bdc21f547c6f03123ef94067f4268f6af2952f73
Dan Grebb <dan@dgrebb.com>
2023-06-26 06:57:02 -0600

    fix(front): removes characters from api endpoint

--- front/src/routes/privacy/+page.server.js
+++ front/src/routes/privacy/+page.server.js
@@ -2,7 +2,7 @@ import { error } from "@sveltejs/kit";
 import api from "../../api";
 import { PUBLIC_API_URL, PUBLIC_API_PATH_PRIVACY } from "$env/static/public";
 
-const privacyEndpoint = `${PUBLIC_API_URL}${PUBLIC_API_PATH_PRIVACY}/as`;
+const privacyEndpoint = `${PUBLIC_API_URL}${PUBLIC_API_PATH_PRIVACY}`;
 
 export async function load({ params }) {
   const privacyContent = await api(privacyEndpoint);
