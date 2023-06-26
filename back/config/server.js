9281276cb89e0bb76f69b22e4749e9354961c884
Dan Grebb <dan@dgrebb.com>
2023-06-26 17:28:04 -0400

    feat(strapi): adds custom webhook body and patch for strapi
    closes #218 and #78

--- back/config/server.js
+++ back/config/server.js
@@ -1,3 +1,6 @@
+const URL = process.env.PUBLIC_ENV;
+const ref = URL === "production" ? "main" : "develop";
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -6,5 +9,12 @@ module.exports = ({ env }) => ({
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    defaultHeaders: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${env("WEBHOOK_TOKEN")}`,
+    },
+    extraBody: {
+      ref,
+    },
   },
 });
