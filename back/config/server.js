34e2192aab66b73a0d8e1c7a9f1bca42a513c622
Dan Grebb <dan@dgrebb.com>
2023-06-26 17:41:16 -0400

    build(strapi): renames github dispatch token var

--- back/config/server.js
+++ back/config/server.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
     defaultHeaders: {
       Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${env("WEBHOOK_TOKEN")}`,
+      Authorization: `Bearer ${env("DISPATCH_TOKEN")}`,
     },
     extraBody: {
       ref,
