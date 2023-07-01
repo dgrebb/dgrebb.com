344526fe3d2c21f9078bb12a8f7b7d1b473bac02
Dan Grebb <dan@dgrebb.com>
2023-07-01 16:29:34 -0400

    feat(front): adds page transitions
    adjustments for layout shift; additional flourish creates illusion that top border never fades

---
+++ front/src/components/PageTransition.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { browser } from "$app/environment";
+  import { fade } from "svelte/transition";
+  
+  export let pathname;
+
+  function animateOut() {
+    if (browser) document.body.classList.add("animating");
+  }
+
+  function animateIn() {
+    setTimeout(() => {
+      if (browser) document.body.classList.remove("animating");
+    }, 100);
+  }
+
+</script>
+
+{#key pathname}
+  <div
+    transition:fade={{ duration: 300, delay: 50 }}
+    on:outrostart={animateOut}
+    on:introend={animateIn}
+  >
+    <slot />
+  </div>
+{/key}
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -7,53 +7,54 @@
   import Links from "../components/Links.svelte";
   import Link from "../components/markdown/Link.svelte";
   import Flourish from "../layout/Flourish.svelte";
+  import PageTransition from "../components/PageTransition.svelte";
 
   export let data;
-  const { seo, headline, intro, links } = data;
+  const { seo, headline, intro, links, pathname } = data;
   const image = seo?.metaImage?.data?.attributes.formats.small || { url: "/bio.jpg", alt: "A picture of Dan smiling" };
   const metaSocialOG = seo?.metaSocial.find((obj) => obj.socialNetwork === "Facebook");
 </script>
 
-<section class="bio text-center">
-  <Flourish />
-  {#if image}
-    <Image
-      src={`${PUBLIC_MEDIA_URL}${image.url}`}
-      alt={image.alternativeText}
-      title="Hi!"
-      classes="bio-picture"
-      ariaHidden={true}
-    />
-    <noscript>
-      <img
+<PageTransition {pathname}>
+  <section class="bio text-center">
+    <Flourish />
+    {#if image}
+      <Image
         src={`${PUBLIC_MEDIA_URL}${image.url}`}
         alt={image.alternativeText}
         title="Hi!"
         classes="bio-picture"
         ariaHidden={true}
       />
-    </noscript>
-  {/if}
-  <h1 class="headline">{headline}</h1>
-  <SvelteMarkdown renderers={{ link: Link }} source={intro} />
-</section>
-{#if links.length}
-  <section class="links">
-    <Links {links} />
+      <noscript>
+        <img
+          src={`${PUBLIC_MEDIA_URL}${image.url}`}
+          alt={image.alternativeText}
+          title="Hi!"
+          classes="bio-picture"
+          ariaHidden={true}
+        />
+      </noscript>
+    {/if}
+    <h1 class="headline">{headline}</h1>
+    <SvelteMarkdown renderers={{ link: Link }} source={intro} />
   </section>
-{/if}
+  {#if links.length}
+    <section class="links">
+      <Links {links} />
+    </section>
+  {/if}
+</PageTransition>
 
 <Head>
-  <meta
-    name="viewport"
-    content={seo?.metaViewport || "width=device-width, initial-scale=1"}
-  />
+  <meta name="viewport" content={seo?.metaViewport || 'width=device-width, initial-scale=1'} />
   <meta name="thumbnail" content={`${PUBLIC_MEDIA_URL}${image.url}`} />
 </Head>
 
 <MetaTags
-  title={seo.metaTitle || "Dan Grebb"}
-  description={seo.metaDescription || "Dan Grebb is a Software Engineer from Philadelphia, Pennsylvania."}
+  title={seo.metaTitle || 'Dan Grebb'}
+  description={seo.metaDescription ||
+    'Dan Grebb is a Software Engineer from Philadelphia, Pennsylvania.'}
   openGraph={metaSocialOG?.image
     ? {
         images: [
