36fdca7468396992f3219bb4931da2e3248707b8
Dan Grebb <dan@dgrebb.com>
2023-07-08 11:53:25 -0400

    build(front): adds html to footer

--- front/src/layout/Footer.svelte
+++ front/src/layout/Footer.svelte
@@ -6,7 +6,7 @@
 
 <footer class="footer">
   <small>
-    {#if copyleft}{copyleft}{/if}
+    {#if copyleft}{@html copyleft}{/if}
     {year} AD 
     | {#if copyright}{copyright}{/if}
     | <a href="/privacy">Privacy Policy</a>
