70d63087ac57f5ada6d83fe30f06d8d586b3b7fc
Dan Grebb <dan@dgrebb.com>
2023-07-09 02:23:12 -0400

    feat(front): adds lazyloading to post(s) page images
    closes #401

--- front/src/routes/posts/+page.svelte
+++ front/src/routes/posts/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from "svelte";
   import { PUBLIC_MEDIA_URL } from "$env/static/public";
   import Flourish from "../../layout/Flourish.svelte";
   import PageTransition from "../../components/PageTransition.svelte";
@@ -6,31 +7,35 @@
   import Link from "../../components/content/renderers/Link.svelte";
 
   export let data;
+  let mounted = false;
   const { pathname } = data;
   let image,
-    blurhash,
+    thumbnail,
     seo = false;
   const { headline, description } = data.postsPageContent;
   seo = data?.postsPageContent?.seo;
   const posts = [];
 
   data.posts.map((post) => {
-    const pageContent = post.attributes;
-    const { title, slug, seo } = pageContent;
-    const thumbnail = pageContent.hero?.data || false;
+    const { title, slug, hero } = post.attributes;
 
-    if (thumbnail) {
-      image = `${PUBLIC_MEDIA_URL}${thumbnail.attributes.formats.medium.url}`;
-      blurhash = thumbnail.attributes?.blurhash;
-    }
+    const heroImages = hero?.data?.attributes;
+    const full = heroImages?.url || false;
+    const { thumbnail } = heroImages?.formats || false;
+    const image = full ? `${PUBLIC_MEDIA_URL}${full.url}` : false;
+    const lazyImage = thumbnail ? `${PUBLIC_MEDIA_URL}${thumbnail.url}` : false;
 
     posts.push({
       title,
       slug,
       seo,
       image,
-      blurhash,
+      lazyImage,
     });
+  });
+
+  onMount(() => {
+    mounted = true;
   });
 </script>
 
@@ -46,7 +51,9 @@
       {#each posts as post}
         <li
           class="post-item"
-          style={`background-image: url('${post.image ? post.image : null}')`}
+          style={post.lazyImage
+            ? `background-image: url('${post.lazyImage}');`
+            : null}
         >
           <a href={`/post/${post.slug}`} class="post-link">
             {post.title}
@@ -56,3 +63,11 @@
     </ul>
   </section>
 </PageTransition>
+
+<svelte:head>
+  {#if mounted}
+    {#each posts as post}
+      <link rel="preload" as="image" href={post.image} />
+    {/each}
+  {/if}
+</svelte:head>
