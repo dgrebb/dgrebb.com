425c0444b0eaf392678a857561639146342570d7
Dan Grebb <dan@dgrebb.com>
2023-07-12 18:37:45 -0400

    refactor(front): adds svelte 404 page, updates logging, adds styles for static 404.html

---
+++ front/src/routes/404/+page.server.js
@@ -0,0 +1,16 @@
+import * as Sentry from "@sentry/sveltekit";
+import { PUBLIC_ENV, PUBLIC_SENTRY_DSN } from "$env/static/public";
+
+console.log("404 - Not Found ~ /404 handler");
+
+Sentry.captureMessage("Page Not Found", {
+  environment: PUBLIC_ENV,
+  beforeSend(event) {
+    if (event.user) {
+      delete event.user.ip;
+    }
+    if (event.server_name) {
+      delete event.server_name;
+    }
+  },
+});
