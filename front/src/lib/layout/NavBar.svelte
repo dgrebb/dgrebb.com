22f717790017830986617e91f5f4f6fef725cb73
Dan Grebb <dan@dgrebb.com>
2023-07-14 18:49:45 -0400

    build(front): adds active class for posts nav item

--- front/src/lib/layout/NavBar.svelte
+++ front/src/lib/layout/NavBar.svelte
@@ -1,6 +1,8 @@
 <script>
+  import { page } from "$app/stores";
   // import NavToggle from "../components/NavToggle.svelte";
   export let navItems;
+  $: postsActive = $page.route.id.includes("post");
 </script>
 
 <nav class="nav-bar">
@@ -8,7 +10,7 @@
   <ul class="nav-list">
     {#each navItems as { href, title }}
       <li class="nav-item">
-        <a class="nav-link" {href}>{title}</a>
+        <a class={`nav-link ${postsActive ? "active" : ""}`} {href}>{title}</a>
       </li>
     {/each}
   </ul>
