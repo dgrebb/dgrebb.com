5ce5bc8243beb550c6f977b52aeb07c76b63e7ad
Dan Grebb <dan@dgrebb.com>
2023-07-15 17:15:04 -0400

    feat(front): adds category listing page

--- front/src/routes/posts/category/[category]/+page.server.js
+++ front/src/routes/posts/category/[category]/+page.server.js
@@ -1,11 +1,31 @@
 import { error } from "@sveltejs/kit";
-import api from "@api";
+import api, { categoryAPI } from "@api";
 import { PUBLIC_API_URL as URL } from "$env/static/public";
 
 export const trailingSlash = "always";
 
 export async function load({ params }) {
+  const { category } = params;
+  const categoryEndpoint = URL + `/category/${category}`;
+  const postsEndpoint =
+    URL +
+    `/posts/?pagination[page]=1&pagination[pageSize]=10&sort[0]=createdAt:desc&filters[categories][slug][$eq]=${category}&populate=hero`;
+  const [content, posts] = await Promise.all([
+    categoryAPI(categoryEndpoint),
+    api(postsEndpoint),
+  ]);
+
+  if (!content) {
+    console.error({
+      route: route.id,
+      endpoint: pageEndpoint,
+      error: page.error,
+    });
+  }
+
   return {
-    posts: [],
+    content,
+    posts: [...posts] || [],
   };
+  
 }
