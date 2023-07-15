892167290f18ec237b73d2f3d6578733cd761eab
Dan Grebb <dan@dgrebb.com>
2023-07-15 14:20:41 -0400

    build(strapi): adds category slug endpoint and category description field

--- back/src/api/category/controllers/category.js
+++ back/src/api/category/controllers/category.js
@@ -21,10 +21,6 @@ module.exports = createCoreController(
           "api::category.category",
           query
         );
-        // const schema = strapi.getModel("api::category.category");
-        // const sanitizedEntity = await sanitize.contentAPI.output(category, schema);
-
-        // return this.transformResponse(sanitizedEntity[0]);
 
         return category;
       } catch (error) {
