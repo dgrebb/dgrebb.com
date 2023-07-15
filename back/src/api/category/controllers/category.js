ed6e70657ecff01714ad3fe32d6e19382a98601f
Dan Grebb <dan@dgrebb.com>
2023-07-15 10:59:37 -0400

    build(strapi): adds route and controller for category lookup by slug

--- back/src/api/category/controllers/category.js
+++ back/src/api/category/controllers/category.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * category controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController(
+  "api::category.category",
+  ({ strapi }) => ({
+    async findBySlug(ctx) {
+      try {
+        const { slug } = ctx.params;
+        const query = {
+          filters: { slug },
+          ...ctx.query,
+        };
+
+        const category = await strapi.entityService.findMany(
+          "api::category.category",
+          query
+        );
+        // const schema = strapi.getModel("api::category.category");
+        // const sanitizedEntity = await sanitize.contentAPI.output(category, schema);
+
+        // return this.transformResponse(sanitizedEntity[0]);
+
+        return category;
+      } catch (error) {
+        ctx.body = error;
+      }
+    },
+  })
+);
