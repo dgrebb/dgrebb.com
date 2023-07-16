6956431ea7c2fc9ac5ede909fb048bfa1ff2ed38
Dan Grebb <dan@dgrebb.com>
2023-07-15 21:58:50 -0400

    build(front): adds api path via vars and throws 404s if category does not exist

--- front/src/routes/posts/category/[category]/+page.server.js
+++ front/src/routes/posts/category/[category]/+page.server.js
@@ -1,25 +1,31 @@
 import { error } from "@sveltejs/kit";
 import api, { categoryAPI } from "@api";
-import { PUBLIC_API_URL as URL, PUBLIC_API_PATH_CATEGORY as CAT, PUBLIC_CATEGORY_PAGE_PARAMS as PARAMS } from "$env/static/public";
+import {
+  PUBLIC_API_URL as URL,
+  PUBLIC_API_PATH_POSTS as POSTS,
+  PUBLIC_API_PATH_CATEGORY as CAT,
+  PUBLIC_CATEGORY_PAGE_PARAMS as PARAMS,
+} from "$env/static/public";
 
 export const trailingSlash = "always";
 
 export async function load({ params }) {
   const { category } = params;
-  const categoryEndpoint = URL + URL + PARAMS + category;
-  const postsEndpoint =
-    URL +
-    `/posts/?pagination[page]=1&pagination[pageSize]=10&sort[0]=createdAt:desc&filters[categories][slug][$eq]=${category}&populate=hero`;
+  const categoryEndpoint = URL + CAT + category;
+  const postsEndpoint = URL + POSTS + PARAMS;
   const [content, posts] = await Promise.all([
     categoryAPI(categoryEndpoint),
     api(postsEndpoint),
   ]);
 
   if (!content) {
-    console.error({
-      route: route.id,
-      endpoint: pageEndpoint,
-      error: page.error,
+    console.info({
+      params,
+      postsEndpoint,
+      categoryEndpoint,
+    });
+    throw error(404, {
+      message: "Category not found!",
     });
   }
 
@@ -27,5 +33,4 @@ export async function load({ params }) {
     content,
     posts: [...posts] || [],
   };
-  
 }
