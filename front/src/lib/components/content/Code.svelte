2d0c873c82c3677d564374c66887982ca2732975
Dan Grebb <dan@dgrebb.com>
2023-07-16 12:33:35 -0400

    fix(front): adds default language to `Code` component; adds `highlightedLines` support for upcoming implementatio n

--- front/src/lib/components/content/Code.svelte
+++ front/src/lib/components/content/Code.svelte
@@ -1,20 +1,37 @@
 <script>
   import Highlight, { LineNumbers } from "svelte-highlight";
+  import plaintext from "svelte-highlight/languages/plaintext";
   import JavaScript from "svelte-highlight/languages/javascript";
 
   export let lang;
   export let text;
   export let title = null;
-  let language = lang.toLowerCase();
+  export let highlightedLines = false;
+  let langLower = lang.toLowerCase();
 
   let which = {
-    "javascript": JavaScript,
-    "hcl": JavaScript
-  }
+    javascript: JavaScript,
+    hcl: JavaScript,
+  };
 
+  let language = which[langLower] || plaintext;
 </script>
 
-<div class="syntax-highlighter">
-  <span class="title">{title ? title : lang}</span>
-  <Highlight code={text} language={which[language]} />
-</div>
+{#if highlightedLines}
+  <div class="syntax-highlighter">
+    <span class="title">{title ? title : lang}</span>
+    <Highlight code={text} {language} let:highlighted>
+      <LineNumbers
+        {highlighted}
+        {highlightedLines}
+        --highlighted-background="#fefefe"
+        hideBorder
+      />
+    </Highlight>
+  </div>
+{:else}
+  <div class="syntax-highlighter">
+    <span class="title">{title ? title : lang}</span>
+    <Highlight code={text} {language} />
+  </div>
+{/if}
