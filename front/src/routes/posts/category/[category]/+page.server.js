380b91159082b0c92319dd0d4e554166ab100836
Dan Grebb <dan@dgrebb.com>
2023-07-15 20:45:39 -0400

    build(front): adds category api env vars

--- front/src/routes/posts/category/[category]/+page.server.js
+++ front/src/routes/posts/category/[category]/+page.server.js
@@ -1,12 +1,12 @@
 import { error } from "@sveltejs/kit";
 import api, { categoryAPI } from "@api";
-import { PUBLIC_API_URL as URL } from "$env/static/public";
+import { PUBLIC_API_URL as URL, PUBLIC_API_PATH_CATEGORY as CAT, PUBLIC_CATEGORY_PAGE_PARAMS as PARAMS } from "$env/static/public";
 
 export const trailingSlash = "always";
 
 export async function load({ params }) {
   const { category } = params;
-  const categoryEndpoint = URL + `/category/${category}`;
+  const categoryEndpoint = URL + URL + PARAMS + category;
   const postsEndpoint =
     URL +
     `/posts/?pagination[page]=1&pagination[pageSize]=10&sort[0]=createdAt:desc&filters[categories][slug][$eq]=${category}&populate=hero`;
