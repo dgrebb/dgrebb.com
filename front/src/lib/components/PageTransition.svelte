087b58e38882b402462683f30d99cca5c3a60e34
Dan Grebb <dan@dgrebb.com>
2023-07-23 11:08:54 -0400

    fix(front): cleans up page transitions with bg color set to vh instead of %
    closes #483

--- front/src/lib/components/PageTransition.svelte
+++ front/src/lib/components/PageTransition.svelte
@@ -1,25 +1,24 @@
 <script>
-  import { browser } from "$app/environment";
   import { fade } from "svelte/transition";
-  
+  import { scrollTop } from "@utils";
+
   export let pathname;
 
   function animateOut() {
-    if (browser) document.body.classList.add("animating");
+    document.body.classList.add("animating");
   }
 
   function animateIn() {
     setTimeout(() => {
-      if (browser) document.body.classList.remove("animating");
-    }, 100);
+      document.body.classList.remove("animating");
+    }, 333);
   }
-
 </script>
 
 {#key pathname}
   <div
     class="transition-container"
-    transition:fade={{ duration: 300, delay: 50 }}
+    transition:fade={{ duration: 333, delay: 50 }}
     on:outrostart={animateOut}
     on:introend={animateIn}
   >
