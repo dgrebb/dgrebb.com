dee8471a14800d23c2540ce0ffdefbd301592fa0
Dan Grebb <dan@dgrebb.com>
2023-07-25 22:11:50 -0400

    feat(front): adds page transitions and layout for category page

--- front/src/lib/components/SamePageTransition.svelte
+++ front/src/lib/components/SamePageTransition.svelte
@@ -6,22 +6,28 @@
   export let transitionKey;
   export let classList = false;
 
+  function doIt() {
+    return {
+      duration: 333,
+    };
+  }
+
   function animateOut() {
     document.body.classList.add("animating-page");
   }
 
   function animateIn() {
     setTimeout(() => {
+      document.body.classList.remove("animating");
       document.body.classList.remove("animating-page");
     }, 333);
   }
-
 </script>
 
-{#key (transitionKey)}
+{#key transitionKey}
   <div
     class="page-transition-container {classList ? classList : ''}"
-    transition:fade={{ duration: 333, delay: 50 }}
+    transition:doIt
     on:outrostart={animateOut}
     on:introend={animateIn}
   >
--- front/src/routes/posts/category/[category]/+page.svelte
+++ front/src/routes/posts/category/[category]/+page.svelte
@@ -15,14 +15,13 @@
     categories,
     posts,
     pageMeta,
-    pathname,
   } = data);
 </script>
 
-<PageTransition transitionKey={route}>
+<PageTransition transitionKey={route} classList="category-page">
   <section class="category">
     <div class="category-summary">
-      <SamePageTransition transitionKey={pathname}>
+      <SamePageTransition transitionKey={name}>
         <a id="main">Main Content</a>
         <h1 class="category-name">{name}</h1>
         {#if description}
@@ -32,32 +31,38 @@
     </div>
     <div class="category-posts-list">
       <Flourish />
-      {#if posts}
-        <ul>
-          {#each posts as { attributes: { title, publishedAt, slug, summary, position, hero: { alternativeText, data: { attributes: { formats: { thumbnail, small, medium } } } } } }, i}
-            {@const date = new Date(publishedAt).toDateString()}
-            <li class="post-item">
-              <a href="/post/{slug}">
-                <div class="post-item-heading">
-                  <h2 class="post-title">{title}</h2>
-                  <time datetime={date} class="post-date">{date}</time>
-                </div>
-                <div
-                  class="post-item-image"
-                  style="background-image: url('{thumbnail?.url}'); {position && `background-position: ${position};`}"
-                />
-              </a>
-              {#if summary}
-                <div class="post-item-summary">
-                  <SvelteMarkdown renderers={{ link: Link }} source={summary} />
-                </div>
-              {/if}
-            </li>
-          {/each}
-        </ul>
-      {:else}
-        <h2>There aren't any posts yet! Come back soon.</h2>
-      {/if}
+      <SamePageTransition transitionKey={name}>
+        {#if posts}
+          <ul>
+            {#each posts as { attributes: { title, publishedAt, slug, summary, position, hero: { alternativeText, data: { attributes: { formats: { thumbnail, small, medium } } } } } }, i}
+              {@const date = new Date(publishedAt).toDateString()}
+              <li class="post-item">
+                <a href="/post/{slug}">
+                  <div class="post-item-heading">
+                    <h2 class="post-title">{title}</h2>
+                    <time datetime={date} class="post-date">{date}</time>
+                  </div>
+                  <div
+                    class="post-item-image"
+                    style="background-image: url('{thumbnail?.url}'); {position &&
+                      `background-position: ${position};`}"
+                  />
+                </a>
+                {#if summary}
+                  <div class="post-item-summary">
+                    <SvelteMarkdown
+                      renderers={{ link: Link }}
+                      source={summary}
+                    />
+                  </div>
+                {/if}
+              </li>
+            {/each}
+          </ul>
+        {:else}
+          <h2>There aren't any posts yet! Come back soon.</h2>
+        {/if}
+      </SamePageTransition>
     </div>
     <CategoryAside {categories} />
   </section>
