d6286ff1b93db8c41d0fe1662c26646e68dbdc21
Dan Grebb <dan@dgrebb.com>
2023-07-29 12:23:54 -0400

    fix(front): adjusts footnotes indentation and adds nofollow/noreferrer/target=_blank to links
    closes #503

--- front/src/lib/components/content/renderers/Footnotes.svelte
+++ front/src/lib/components/content/renderers/Footnotes.svelte
@@ -5,6 +5,6 @@
 <h2 class="footnotes-title">Footnotes</h2>
 <ul class="footnotes">
   {#each footnotes as { link, title }, i}
-    <li class="note"><a href={link}>{title}</a></li>
+    <li class="note"><a href={link} rel="noopener noreferrer" target="_blank">{title}</a></li>
   {/each}
 </ul>
