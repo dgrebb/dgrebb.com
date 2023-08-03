9fae154f77b45e1c779fd3ed42298f8cba54c275
Dan Grebb <dan@dgrebb.com>
2023-08-03 04:43:25 -0400

    build(front): adds syntaxes to `Code` component

--- front/src/lib/components/content/Code.svelte
+++ front/src/lib/components/content/Code.svelte
@@ -2,6 +2,8 @@
   import Highlight, { LineNumbers } from "svelte-highlight";
   import plaintext from "svelte-highlight/languages/plaintext";
   import JavaScript from "svelte-highlight/languages/javascript";
+  import XML from "svelte-highlight/languages/xml";
+  import CSS from "svelte-highlight/languages/css";
   import CodeCopy from "./CodeCopy.svelte";
 
   export let lang;
@@ -13,6 +15,8 @@
 
   let which = {
     javascript: JavaScript,
+    html: XML,
+    css: CSS,
     hcl: JavaScript,
   };
 
