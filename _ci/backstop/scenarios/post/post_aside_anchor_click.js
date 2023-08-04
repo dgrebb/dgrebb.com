a96f9863e0efb63ac1b14b2b14f3767e56f39545
Dan Grebb <dan@dgrebb.com>
2023-08-03 21:42:55 -0400

    ci(backstop): adds `post_aside_anchor_click` scenario and test refs

--- _ci/backstop/scenarios/post/post_aside_anchor_click.js
+++ _ci/backstop/scenarios/post/post_aside_anchor_click.js
@@ -1,20 +1,22 @@
 const { URL } = require('../../vars');
 const POST_URL = `${URL}/post/chicken-caesar-salad-an-overrated-concoction-of-lettuce-and-delusion/`;
+const { aboveSmallViewports } = require('../../vars.js');
 
 module.exports = {
   label: 'Post - Aside Anchor Click',
   // cookiePath: "backstop/backstop_data/engine_scripts/cookies.json",
   url: POST_URL,
+  viewports: aboveSmallViewports,
   readyEvent: '',
   readySelector: '',
-  delay: 800,
-  onReadyScript: 'puppet/onReadyPosts.js',
+  delay: 0,
   hideSelectors: [],
   removeSelectors: [],
-  clickSelector: '.post-aside a[href="#step-2-caesar-dressing-the-holy-grail-of-salad-sauces"]',
+  clickSelector:
+    '.post-aside .page-navigation-list a[href="#step-2-caesar-dressing-the-holy-grail-of-salad-sauces"]',
   postInteractionWait: 1000,
   selectors: [],
-  selectorExpansion: false,
+  selectorExpansion: true,
   expect: 0,
   misMatchThreshold: 0.2,
   requireSameDimensions: true,
