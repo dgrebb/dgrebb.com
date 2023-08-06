a225b83ffddb76cec8240e9d33eb8fa0150c6596
Dan Grebb <dan@dgrebb.com>
2023-08-06 00:12:28 -0400

    build(backstop): updates home theme switch scenario and refs

--- _ci/backstop/bd/engine_scripts/playwright/onReady.js
+++ _ci/backstop/bd/engine_scripts/playwright/onReady.js
@@ -3,6 +3,4 @@ module.exports = async (page, scenario, viewport, isReference, browserContext) =
   await require('./clickAndHoverHelper')(page, scenario);
   await require('./colorScheme')(page, scenario);
   await require('./overrideCSS')(page, scenario);
-
-  // add more ready handlers here...
 };
