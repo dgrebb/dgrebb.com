6e771c1841ae65b5c380649ab9397efbb016997f
Dan Grebb <dan@dgrebb.com>
2023-08-06 00:21:06 -0400

    build(backstop): sets dark as default `colorScheme` for all scenarios

--- _ci/backstop/bd/engine_scripts/playwright/colorScheme.js
+++ _ci/backstop/bd/engine_scripts/playwright/colorScheme.js
@@ -1,6 +1,8 @@
 module.exports = async (page, scenario) => {
   if (scenario.colorScheme) {
     await page.emulateMedia({ colorScheme: scenario.colorScheme });
+  } else {
+    await page.emulateMedia({ colorScheme: 'dark' });
   }
 
   console.log('Updated color scheme for: ' + scenario.label);
