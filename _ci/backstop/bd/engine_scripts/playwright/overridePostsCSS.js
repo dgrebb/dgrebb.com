e7bf35f4c631c52b287b2afd64bab569807fd74d
Dan Grebb <dan@dgrebb.com>
2023-08-06 13:13:23 -0400

    build(backstop): updates posts onready script and css overrides

--- _ci/backstop/bd/engine_scripts/playwright/overridePostsCSS.js
+++ _ci/backstop/bd/engine_scripts/playwright/overridePostsCSS.js
@@ -1,13 +1,24 @@
-const BACKSTOP_TEST_CSS_OVERRIDE = `* {transition: none !important; transition-duration: 0 !important; transition-delay: 0 !important;} html {background-image: none; overflow: auto !important; min-height: 1500px !important; height: 1500px !important;} body {overflow: visible !important; min-height: 1500px !important; height: 1500px !important;}`;
+const BACKSTOP_TEST_CSS_OVERRIDE = `
+  * {
+    transition: none !important; 
+    transition-duration: 0 !important; 
+    transition-delay: 0 !important;
+  } 
+  html {
+  } 
+  body {
+    overflow: visible !important;
+    position: absolute !important;
+    top: 0 !important;
+    bottom: 0 !important;
+    left: 0 !important;
+    right: 0 !important;
+  }
+`;
 
 module.exports = async (page, scenario) => {
-  // inject arbitrary css to override styles
-  await page.evaluate(`window._styleData = '${BACKSTOP_TEST_CSS_OVERRIDE}'`);
-  await page.evaluate(() => {
-    const style = document.createElement('style');
-    const styleNode = document.createTextNode(window._styleData);
-    style.appendChild(styleNode);
-    document.head.appendChild(style);
+  await page.addStyleTag({
+    content: BACKSTOP_TEST_CSS_OVERRIDE,
   });
 
   console.log('BACKSTOP_TEST_CSS_OVERRIDE injected for: ' + scenario.label);
