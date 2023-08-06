222f73a988d99916afc6aee9913f4821684839ba
Dan Grebb <dan@dgrebb.com>
2023-08-06 18:22:13 -0400

    build(backstop): defines viewports as importable vars; updates posts-grid hover first scenario and refs

--- _ci/backstop/scenarios/posts/posts_hover_post_1.js
+++ _ci/backstop/scenarios/posts/posts_hover_post_1.js
@@ -1,12 +1,13 @@
-const { URL } = require('../vars');
+const { URL, sm, aboveSmallViewports } = require('../vars');
 const POSTS_URL = `${URL}/posts`;
 
 module.exports = {
   label: 'Posts - Hover First Post',
   url: POSTS_URL,
+  viewports: [sm, ...aboveSmallViewports],
   onReadyScript: 'playwright/onReadyPosts.js',
-  hoverSelector: '.posts-grid li:nth-child(1) .post-link',
-  postInteractionWait: 2500,
+  hoverSelector: '.posts-grid li:first-of-type .post-link',
+  postInteractionWait: 800,
   selectors: ['document'],
   misMatchThreshold: 0.2,
   requireSameDimensions: true,
