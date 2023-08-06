cbedf9b844284fffd88e1d9015cdba2e84501478
Dan Grebb <dan@dgrebb.com>
2023-08-06 18:31:27 -0400

    build(backstop): updates scenario and refs for posts-grid hover seventh

--- _ci/backstop/scenarios/posts/posts_hover_post_7.js
+++ _ci/backstop/scenarios/posts/posts_hover_post_7.js
@@ -1,13 +1,13 @@
-const { URL } = require('../vars');
+const { URL, md, xl, xxl } = require('../vars');
 const POSTS_URL = `${URL}/posts`;
 
 module.exports = {
   label: 'Posts - Hover Seventh Post',
   url: POSTS_URL,
+  viewports: [md, xl, xxl],
   onReadyScript: 'playwright/onReadyPosts.js',
-  delay: 1000,
   hoverSelector: '.posts-grid li:nth-child(7) .post-link',
-  postInteractionWait: 2500,
+  postInteractionWait: 800,
   selectors: ['document'],
   selectorExpansion: false,
   misMatchThreshold: 0.2,
