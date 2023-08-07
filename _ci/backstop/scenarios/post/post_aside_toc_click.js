c93be3dcb45b19b2e346143029e3ebbe92c11d38
Dan Grebb <dan@dgrebb.com>
2023-08-07 17:38:14 -0400

    build(backstop): updates names of aside toc click/active scenarios

--- _ci/backstop/scenarios/post/post_aside_toc_click.js
+++ _ci/backstop/scenarios/post/post_aside_toc_click.js
@@ -3,20 +3,14 @@ const POST_URL = `${URL}/post/chicken-caesar-salad-an-overrated-concoction-of-le
 const { aboveSmallViewports } = require('../vars.js');
 
 module.exports = {
-  label: 'Post - Aside Anchor Click',
+  label: 'Post - Aside TOC Click',
   url: POST_URL,
   viewports: aboveSmallViewports,
-  readyEvent: '',
-  readySelector: '',
-  delay: 0,
-  hideSelectors: [],
-  removeSelectors: [],
   clickSelector:
     '.post-aside .page-navigation-list a[href="#step-2-caesar-dressing-the-holy-grail-of-salad-sauces"]',
-  postInteractionWait: 1000,
-  selectors: [],
-  selectorExpansion: true,
-  expect: 0,
+  postInteractionWait: 800,
+  selectors: ['viewport'],
+  selectorExpansion: false,
   misMatchThreshold: 0.2,
   requireSameDimensions: true,
 };
