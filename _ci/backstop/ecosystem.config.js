172b43f96c71a560c812b3ed871b724a79d63ebc
Dan Grebb <dan@dgrebb.com>
2023-08-07 17:39:07 -0400

    build(backstop): updates standup/teardown for pm2 tasks

--- _ci/backstop/ecosystem.config.js
+++ _ci/backstop/ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: "UI http-server :8080",
-      namespace: "backstop",
-      script: "npm",
-      args: "run serve",
+      name: 'UI http-server :8080',
+      namespace: 'backstop',
+      script: 'npm',
+      args: 'run serve',
     },
     {
-      name: "BackstopJS Remote :3000",
-      namespace: "backstop",
-      script: "npm",
-      args: "run remote",
+      name: 'BackstopJS Remote :3000',
+      namespace: 'backstop',
+      script: 'npm',
+      args: 'run remote',
       watch: ['scenarios', 'bd/engine_scripts'],
       watch_delay: 1000,
       env: {
-        "ENVIRONMENT": process.env.ENVIRONMENT,
-      }
+        ENVIRONMENT: process.env.ENVIRONMENT,
+      },
     },
   ],
 };
