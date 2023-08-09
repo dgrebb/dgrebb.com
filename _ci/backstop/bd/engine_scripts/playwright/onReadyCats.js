5f541dc08fbb3890caf7ebf407a31dcf9fd6a263
Dan Grebb <dan@dgrebb.com>
2023-08-09 15:22:05 -0400

    build(backstop): updates tests for categories page and states

--- _ci/backstop/bd/engine_scripts/playwright/onReadyCats.js
+++ _ci/backstop/bd/engine_scripts/playwright/onReadyCats.js
@@ -3,5 +3,4 @@ module.exports = async (page, scenario, viewport, isReference, browserContext) =
   await require('./clickAndHoverHelper')(page, scenario);
   await require('./colorScheme')(page, scenario);
   await require('./overrideCatsCSS')(page, scenario);
-  await require('./expandMiniNav')(page, scenario);
 };
