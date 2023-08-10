383c8faf890c3cc32b937943cd5b48c57591c2fe
Dan Grebb <dan@dgrebb.com>
2023-08-09 21:08:46 -0400

    build(front): adjustments to js configuration and alias resolution

--- front/vite.local.config.js
+++ front/vite.local.config.js
@@ -1,30 +1,44 @@
-import fs from "fs";
-import path from "path";
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import Icons from "unplugin-icons/vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import fs from 'fs';
+import path from 'path';
+import Icons from 'unplugin-icons/vite';
+import { defineConfig } from 'vite';
+import postcss from './postcss.config.js';
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     Icons({
-      compiler: "svelte",
+      compiler: 'svelte',
     }),
   ],
+  resolve: {
+    alias: {
+      '@api': path.resolve('./src/lib/api'),
+      '@components': path.resolve('./src/lib/components'),
+      '@layout': path.resolve('./src/lib/layout'),
+      '@store': path.resolve('./src/lib/store'),
+      '@styles': path.resolve('./src/lib/styles'),
+      '@utils': path.resolve('./src/lib/_utils'),
+    },
+  },
+  css: {
+    postcss,
+  },
   optimizeDeps: {
-    include: ["highlight.js/lib/core"],
+    include: ['highlight.js/lib/core'],
   },
   server: {
     proxy: {
-      "^/uploads/.*": {
-        target: "http://local.cms.dgrebb.com:1337/uploads",
+      '^/uploads/.*': {
+        target: 'http://local.cms.dgrebb.com:1337/uploads',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/uploads/, ""),
+        rewrite: (path) => path.replace(/^\/uploads/, ''),
       },
     },
     https: {
-      key: fs.readFileSync("local.dgrebb.com.key"),
-      cert: fs.readFileSync("local.dgrebb.com.crt"),
+      key: fs.readFileSync('local.dgrebb.com.key'),
+      cert: fs.readFileSync('local.dgrebb.com.crt'),
     },
     port: 443,
   },
