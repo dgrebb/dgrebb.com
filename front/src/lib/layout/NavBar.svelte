a26d527b51a862f14cdaeec1c82f3940e7b6d3f5
Dan Grebb <dan@dgrebb.com>
2023-08-19 23:44:34 -0400

    build(front): adds category page to subnav items for posts navitem

--- front/src/lib/layout/NavBar.svelte
+++ front/src/lib/layout/NavBar.svelte
@@ -1,9 +1,12 @@
 <script>
-  import { page } from "$app/stores";
+  import { page } from '$app/stores';
   // import NavToggle from "../components/NavToggle.svelte";
   export let navItems;
-  $: postsActive = $page?.route?.id?.includes("post") || false;
-  $: postActive = $page?.route?.id === "/post/[slug]" || false;
+  $: postsActive = $page?.route?.id?.includes('post') || false;
+  $: subactive =
+    $page?.route?.id === '/post/[slug]' ||
+    $page?.route?.id === '/posts/category/[category]' ||
+    false;
 </script>
 
 <nav class="nav-bar" aria-label="Site Pages">
@@ -11,7 +14,9 @@
   <ul class="nav-list">
     {#each navItems as { href, title }}
       <li class="nav-item">
-        <a class="nav-link" class:active={postsActive} class:subactive={postActive} {href}>{title}</a>
+        <a class="nav-link" class:active={postsActive} class:subactive {href}
+          >{title}</a
+        >
       </li>
     {/each}
   </ul>
