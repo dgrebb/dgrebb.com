4a608930acab5eae2ddf0cd44ff415fa830c4997
Dan Grebb <dan@dgrebb.com>
2023-08-27 22:29:58 -0400

    refactor(front): refactors naming and exports of uiHelpers

--- front/src/lib/_utils/pokeTrapper.js
+++
@@ -1,8 +0,0 @@
-export const pokeTrapper = {
-  tocClick: (heading) => plausible('TOC Click', { props: { heading } }),
-  categoryClick: (page, category) =>
-    plausible('Category Click', { props: { page, category } }),
-  relatedClick: (page, title) =>
-    plausible('Related Post Click', { props: { page, title } }),
-  themeToggleClick: (theme) => plausible('Theme Toggled', { props: { theme } }),
-};
