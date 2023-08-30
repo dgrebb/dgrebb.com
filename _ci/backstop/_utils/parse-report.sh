17d71f787c067920531ef050c543bc568f137346
Dan Grebb <dan@dgrebb.com>
2023-08-30 16:32:51 -0400

    build(backstop): right-aligns table results for mismatch column

--- _ci/backstop/_utils/parse-report.sh
+++ _ci/backstop/_utils/parse-report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq -c -r '"<table><tr><th align=\"left\">Test</th><th>Viewport</th><th>Mismatch Amount</th></tr>", (.tests[] | select(.status | contains("fail")) | "<tr><td><a href=\"" + .pair.url + "\" target=\"_blank\">" + .pair.label + "</a></td>", "<td>" + .pair.viewportLabel + "</td>", "<td>" + .pair.diff.misMatchPercentage + "</td></tr>"), "</table>"' ./bd/bitmaps_test/*/*/report.json
+jq -c -r '"<table><tr><th align=\"left\">Test</th><th>Viewport</th><th align=\"right\">Mismatch Amount</th></tr>", (.tests[] | select(.status | contains("fail")) | "<tr><td><a href=\"" + .pair.url + "\" target=\"_blank\">" + .pair.label + "</a></td>", "<td align=\"center\">" + .pair.viewportLabel + "</td>", "<td align=\"right\">" + .pair.diff.misMatchPercentage + "</td></tr>"), "</table>"' ./bd/bitmaps_test/*/*/report.json
