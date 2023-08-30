48cef72a689af88607b6c0226d7179ccbe7f8721
Dan Grebb <dan@dgrebb.com>
2023-08-30 16:19:22 -0400

    build(backstop): adds test URL to workflow summary table

--- _ci/backstop/_utils/parse-report.sh
+++ _ci/backstop/_utils/parse-report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq -c -r '"<table><tr><th>Test</th><th>Viewport</th><th>Mismatch Amount</th></tr>", (.tests[] | select(.status | contains("fail")) | "<tr><td>" + .pair.label + "</td>", "<td>" + .pair.viewportLabel + "</td>", "<td>" + .pair.diff.misMatchPercentage + "</td></tr>"), "</table>"' ./bd/bitmaps_test/*/*/report.json
+jq -c -r '"<table><tr><th align=\"left\">Test</th><th>Viewport</th><th>Mismatch Amount</th></tr>", (.tests[] | select(.status | contains("fail")) | "<tr><td><a href=\"" + .pair.url + "\" target=\"_blank\">" + .pair.label + "</a></td>", "<td>" + .pair.viewportLabel + "</td>", "<td>" + .pair.diff.misMatchPercentage + "</td></tr>"), "</table>"' ./bd/bitmaps_test/*/*/report.json
