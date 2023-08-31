02e495aada03d3c75aa4054536673a9c18c787a0
Dan Grebb <dan@dgrebb.com>
2023-08-31 02:02:01 -0400

    build(github): adjusts warmup and `npm run`s environment specific build script

--- _ci/_utils/warmup.sh
+++ _ci/_utils/warmup.sh
@@ -17,7 +17,7 @@
 USERAGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"
 
 prd() {
-    echo "Warming cache for example.com..."
+    echo "Warming cache for www.dgrebb.com..."
     warmUp "www.dgrebb.com"
     echo "www.dgrebb.com cache warmed"
 }
@@ -31,14 +31,14 @@ stg() {
 all() {
     echo "Warming cache for all sites...."
     stg
-    prd
+    # prd
 }
 
 warmUp() {
     # If there are particular subdirectories in your sitemap that you do not wish to parse
     # (i.e. because they cannot be cached), you can use the following regex:
     # grep -oP "https?://$1\/((?!subdirectory))[^<]*"
-    wget --user-agent="$USERAGENT" -q "https://$1/sitemap.xml" --output-document - | egrep -o "https?://[^<]+" | wget -i -
+    wget --user-agent="$USERAGENT" -q "https://$1/sitemap.xml" --output-document - | egrep -o "https?://$1[^<]+" | wget -i -
 }
 
 if [ $# -eq 0 ]; then
