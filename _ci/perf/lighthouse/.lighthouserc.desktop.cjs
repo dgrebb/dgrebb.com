0ab5fbe1d58d1963a23deb1f2525821d4cbeaa7b
Dan Grebb <dan@dgrebb.com>
2023-09-01 00:02:09 -0400

    build(ci): adds util and loops over lighthouse reports; generates landing

---
+++ _ci/perf/lighthouse/.lighthouserc.desktop.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: "lighthouse:default",
+  settings: {
+    psiToken: process.env.PSI_APIKEY,
+    formFactor: "desktop",
+    throttling: {
+      rttMs: 40,
+      throughputKbps: 10 * 1024,
+      cpuSlowdownMultiplier: 1,
+      requestLatencyMs: 0, // 0 means unset
+      downloadThroughputKbps: 0,
+      uploadThroughputKbps: 0,
+    },
+    screenEmulation: {
+      mobile: false,
+      width: 1350,
+      height: 940,
+      deviceScaleFactor: 1,
+      disabled: false,
+    },
+    emulatedUserAgent:
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+  },
+};
