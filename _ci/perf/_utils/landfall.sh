057b206b4a95b7d16af4c584199572330d27ce16
Dan Grebb <dan@dgrebb.com>
2023-09-01 15:05:24 -0400

    build(ci): updates to tf reports and lighthouse index files; updates lighthouse workflow config

--- _ci/perf/_utils/landfall.sh
+++ _ci/perf/_utils/landfall.sh
@@ -7,7 +7,12 @@ TIME=$(date +%H:%M:%S)
 pattern='[._]([^._-]+)-[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}\.report\.html'
 
 echo "
-<html>
+<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <title>Lighthouse Reports</title>
     <head>
         <title>
             Lighthouse Reports
