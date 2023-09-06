0d72d9f6c37ba33ee5d463aa5a2c4f42a386e3a2
Dan Grebb <dan@dgrebb.com>
2023-09-05 22:33:00 -0400

    build(ci): adds favicon to reports site

--- _ci/perf/_utils/landfall.sh
+++ _ci/perf/_utils/landfall.sh
@@ -11,6 +11,7 @@ echo "
   <head>
     <meta charset=\"UTF-8\" />
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"favicon.ico\" />
     <title>Lighthouse Reports</title>
     <head>
         <title>
