296f70d435a3cfaebecc6941c691826b035082f9
Dan Grebb <dan@dgrebb.com>
2023-09-09 11:31:37 -0400

    build(front): SEO updates for  and  componentn

--- front/src/lib/components/Links.svelte
+++ front/src/lib/components/Links.svelte
@@ -1,18 +1,18 @@
 <script>
-  import IconCatchafire from "./icons/Catchafire.svelte";
-  import IconGitHub from "~icons/mdi/github";
-  import IconFacebook from "~icons/mdi/facebook";
-  import IconInstagram from "~icons/mdi/instagram";
-  import IconSoundCloud from "~icons/ph/soundcloud-logo-bold";
-  import IconStackOverflow from "~icons/mdi/stackoverflow";
-  import IconLinkedIn from "~icons/entypo-social/linkedin";
-  import IconTwitter from "~icons/mdi/twitter";
-  import IconMastodon from "~icons/mdi/mastodon";
-  import IconFlickr from "~icons/icomoon-free/flickr2";
-  import IconGoodreads from "~icons/ph/goodreads-logo-fill";
-  import IconPinboard from "~icons/typcn/pin-outline";
-  import IconLink from "~icons/mdi/external-link";
-  
+  import IconCatchafire from './icons/Catchafire.svelte';
+  import IconGitHub from '~icons/mdi/github';
+  import IconFacebook from '~icons/mdi/facebook';
+  import IconInstagram from '~icons/mdi/instagram';
+  import IconSoundCloud from '~icons/ph/soundcloud-logo-bold';
+  import IconStackOverflow from '~icons/mdi/stackoverflow';
+  import IconLinkedIn from '~icons/entypo-social/linkedin';
+  import IconTwitter from '~icons/mdi/twitter';
+  import IconMastodon from '~icons/mdi/mastodon';
+  import IconFlickr from '~icons/icomoon-free/flickr2';
+  import IconGoodreads from '~icons/ph/goodreads-logo-fill';
+  import IconPinboard from '~icons/typcn/pin-outline';
+  import IconLink from '~icons/mdi/external-link';
+
   const icons = {
     IconCatchafire,
     IconGitHub,
@@ -27,7 +27,7 @@
     IconGoodreads,
     IconPinboard,
     IconLink,
-  }
+  };
 
   export let links;
 
@@ -35,21 +35,18 @@
     title,
     url,
     icon: icons[`Icon${icon}`],
-    linkClass: icon?.toLowerCase() || "",
+    linkClass: icon?.toLowerCase() || '',
   }));
 </script>
 
-<ul
-  aria-label="Links to Dan Grebb on The Internet"
-  class="link-list"
->
+<ul aria-label="Links to Dan Grebb on The Internet" class="link-list">
   {#each linkMap as { url, title, linkClass, icon }, i}
     <li class="link">
       <a
         href={url}
         {title}
         target="_blank"
-        rel="noopener noreferrer"
+        rel="me"
         class={`${linkClass}-icon`}
       >
         <svelte:component this={icon || IconLink} />
