185a9bcfef0d0e5bff9f9474622ecaba398c5356
Dan Grebb <dan@dgrebb.com>
2023-09-09 11:31:37 -0400

    build(front): SEO updates for  and  componentn

--- front/src/lib/components/Meta.svelte
+++ front/src/lib/components/Meta.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { MetaTags } from "svelte-meta-tags";
+  import { PUBLIC_ENV as env } from '$env/static/public';
+  import { MetaTags } from 'svelte-meta-tags';
 
   export let pageMeta;
 
@@ -22,53 +23,63 @@
     {
       url: metaImage?.url,
       width: metaImage?.width || 1600,
-      height: metaImage?.width || 900,
+      height: metaImage?.height || 900,
       alt: metaImage?.alternativeText,
     },
   ];
 
   const twitter =
-    metaSocial?.find((obj) => obj.socialNetwork === "Twitter") || false;
+    metaSocial?.find((obj) => obj.socialNetwork === 'Twitter') || false;
 
   const facebook =
-    metaSocial?.find((obj) => obj.socialNetwork === "Facebook") || false;
+    metaSocial?.find((obj) => obj.socialNetwork === 'Facebook') || false;
 </script>
 
 <MetaTags
-  title={metaTitle || "Dan Grebb"}
+  title={metaTitle || 'Dan Grebb'}
   {titleTemplate}
   description={metaDescription ||
-    "Dan Grebb is a Software Engineer from Philadelphia, Pennsylvania."}
-  robots={metaRobots || undefined}
-  canonical={canonicalURL}
+    'Dan Grebb is a Software Engineer from Philadelphia, Pennsylvania.'}
+  robots={env === 'production' || env === 'development'
+    ? metaRobots || 'index, follow'
+    : 'noindex, nofollow'}
+  canonical={canonicalURL || false}
   additionalMetaTags={[
     {
-      name: "viewport",
-      content: metaViewport || "width=device-width, initial-scale=1",
+      name: 'viewport',
+      content: metaViewport || 'width=device-width, initial-scale=1',
     },
     {
-      name: "keywords",
+      name: 'keywords',
       content:
         metaKeywords ||
-        "Dan Grebb, dgrebb, software, engineer, volunteer, craft, developer, web, Philadelphia, Pennsylvania",
+        'Dan Grebb, dgrebb, software, engineer, volunteer, craft, developer, web, Philadelphia, Pennsylvania',
+    },
+    {
+      name: 'publisher',
+      content: 'Dan Grebb (www.dgrebb.com)',
+    },
+    {
+      name: 'author',
+      content: 'Dan Grebb (www.dgrebb.com)',
     },
   ]}
   openGraph={{
     title: metaTitle,
     description: metaDescription,
-    url: canonicalURL,
+    url: canonicalURL || false,
     type: type,
     article: {
-      author: "Dan Grebb",
-      publishedTime: publishedAt ? publishedAt : "0000-01-01T00:00:00.000Z",
-      modifiedTime: updatedAt ? updatedAt : "0000-01-01T03:33:00.000Z",
+      author: 'Dan Grebb',
+      publishedTime: publishedAt ? publishedAt : '0000-01-01T00:00:00.000Z',
+      modifiedTime: updatedAt ? updatedAt : '0000-01-01T03:33:00.000Z',
     },
     images: OGImages,
   }}
   twitter={{
-    site: "@dgrebb",
-    creator: "@dgrebb",
-    cardType: "summary_large_image",
+    site: '@dgrebb',
+    creator: '@dgrebb',
+    cardType: 'summary_large_image',
     title: twitter?.title || pageMeta.title,
     description: twitter?.description || pageMeta.description,
     image: twitter?.image?.data?.attributes?.url || metaImage.url,
