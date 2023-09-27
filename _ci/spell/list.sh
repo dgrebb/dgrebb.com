966377283327d1e271c35ee09f3f0d527ebcdc55
Dan Grebb <dan@dgrebb.com>
2023-09-27 15:27:55 -0400

    feat(ci): adds grammar and spell checking for code/content tests (#701)
    closes #694
    
    Currently spell checking runs on code and content `wget` from STG. This
    feature needs to be further built out to run in workflow automation.
    Creating #700 for this.

---
+++ _ci/spell/list.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+bluebg="\e[44m"
+green="32"
+red="31"
+yellow="33"
+BR="\e[${red}m"
+BG="\e[1;${green}m"
+BY="\e[1;${yellow}m"
+NC="\033[0m" # No Color
+
+# Checks final build spelling
+
+SITEMAP="${1:-https://stg.dgrebb.com/sitemap.xml}"
+urls=$(curl -s $SITEMAP | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
+for url in $urls; do
+  echo
+  echo "=================================================="
+  echo
+  echo "📖 Reading $url..."
+  content=$(wget $url -q -O - | npm run text -- -p human --selectors[] {} :selector=img :format=skip) >/dev/null
+  spelling=$(echo $content | npm run -s spell.web.list -- stdin://$url --quiet)
+  grammar=$(echo "$content" | npm run grammar)
+  echo
+  # sp=$(printf "${BG}✓ All spelling correct!${NC}")
+  # if [[ $spelling ]]; then sp=$(printf "${BY}%s\n\n${NC}${BR}%s${NC}" "Spelling errors found!" "$spelling"); fi
+  # echo -e "$sp"
+  # gr=$(printf "${BG}✓ All grammar correct!${NC}")
+  # if [[ $grammar ]]; then sp=$(printf "${BY}%s\n\n${NC}${BR}%s${NC}" "Grammar issues found!" "$grammar"); fi
+  # echo -e "$gr"
+  echo $grammar
+done
