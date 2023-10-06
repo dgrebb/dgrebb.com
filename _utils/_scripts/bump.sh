fd9a81af3c2d912c9858c7c071763ef4102b8388
Dan Grebb <dan@dgrebb.com>
2023-10-05 22:33:02 -0400

    chore(ci): adjusts bump.sh and .cz.json `bump` and `changelog` patterns

--- _utils/_scripts/bump.sh
+++ _utils/_scripts/bump.sh
@@ -6,12 +6,9 @@ config="_utils/_cliff/config.toml"
 file="CHANGELOG.md"
 str=$(cz bump --dry-run | awk '/tag to create: /')
 ver=${str//tag to create: /}
-branch=$(git branch --show-current)
-[[ $branch != release* ]] && git checkout -b release/$ver
-branch=$(git branch --show-current)
 git cliff -c $config --unreleased --tag $ver --prepend ${file}
 git ls-files --modified
-read -p $'\e[31mWould you like to stage the changelog updates for commit?\e[0m: ' -n 1 -r
+read -p $'\e[31mWould you like to cut the release branch and stage changelog updates?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     read -p $'\e[31mWould you like to undo the changelog updates?\e[0m: ' -n 1 -r
@@ -22,6 +19,9 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     git restore CHANGELOG.md
     exit 1
 fi
+branch=$(git branch --show-current)
+[[ $branch != release* ]] && git checkout -b release/$ver
+branch=$(git branch --show-current)
 git add CHANGELOG.md
 git status
 read -p $'\e[31mDouble check the staged files. Would you like to commit these changes?\e[0m: ' -n 1 -r
@@ -37,13 +37,13 @@ echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
-cdfront
-npm version $ver
-cdback
-npm version $ver
+# cdfront
+# npm version $ver
+# cdback
+# npm version $ver
 cd $directory/../
 git add .
-cz bump
+cz bump --files-only
 echo "${branch} created!"
 echo
 read -p $'\e[31mWould you like to push to origin now?\e[0m: ' -n 1 -r
