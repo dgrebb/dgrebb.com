caffcf6edd159e539cd67fdd059484c552c97db2
Dan Grebb <dan@dgrebb.com>
2023-10-07 00:10:05 -0400

    feat(front): introduces the all categories page, tags, api, and interaction
    adds a redirect for /posts/category/ without slug -> all page
    
    closes #742

--- front/src/routes/posts/category/[category]/+page.svelte
+++ front/src/routes/posts/category/[category]/+page.svelte
@@ -6,12 +6,14 @@
   import TransitionElasticFly from '@components/TransitionElasticFly.svelte';
   import Link from '@components/content/renderers/Link.svelte';
   import Flourish from '@layout/Flourish.svelte';
+  import Tag from '@components/icons/Tag.svelte';
   import SvelteMarkdown from 'svelte-markdown';
   import PageNav from '../../../../lib/components/PageNav.svelte';
 
   export let data;
   const route = $page.route.id;
   $: ({
+    category,
     categoriesSingletonContent: { headline },
     categoryCollectionContent: categories,
     categoryContent: { name },
@@ -50,7 +52,7 @@
       >
         {#if posts && posts.length}
           <ul>
-            {#each posts as { attributes: { title, publishedAt, slug, summary, position, hero: { data: { attributes: { formats: { thumbnail } } } } } }, i}
+            {#each posts as { attributes: { title, publishedAt, slug, summary, categories, position, hero: { data: { attributes: { formats: { thumbnail } } } } } }, i}
               {@const date = new Date(publishedAt).toDateString()}
               <li class="post-item">
                 <a href="/post/{slug}">
@@ -64,6 +66,15 @@
                       `background-position: ${position};`}"
                   />
                 </a>
+                <ul class="category-tags">
+                  {#each categories.data as { attributes: { name, slug } }}
+                    <li class="category-tag" class:active={category === slug}>
+                      <a href="/posts/category/{slug}">
+                        <Tag />{name}
+                      </a>
+                    </li>
+                  {/each}
+                </ul>
                 {#if summary && summary.length}
                   <div class="post-item-summary">
                     <SvelteMarkdown
@@ -82,7 +93,7 @@
     </div>
 
     <aside class="aside">
-      <PageNav {categories} {pathname} {setActiveLink} />
+      <PageNav {categories} {pathname} {setActiveLink} {category} />
     </aside>
   </section>
 </PageTransition>
