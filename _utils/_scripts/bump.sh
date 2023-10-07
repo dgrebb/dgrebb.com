7c12b6575be1c1cd681bc8adca8854a1952d0104
Dan Grebb <dan@dgrebb.com>
2023-10-07 15:29:54 -0400

    fix(ci): adjusts bump prompt and command order for restoring and cutting release

--- _utils/_scripts/bump.sh
+++ _utils/_scripts/bump.sh
@@ -8,43 +8,44 @@ str=$(cz bump --dry-run | awk '/tag to create: /')
 ver=${str//tag to create: /}
 git cliff -c $config --unreleased --tag $ver --prepend ${file}
 git ls-files --modified
-read -p $'\e[31mWould you like to cut the release branch and stage changelog updates?\e[0m: ' -n 1 -r
+read -p $'\e[31mWould you like to cut the release branch now?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    read -p $'\e[31mWould you like to undo the changelog updates?\e[0m: ' -n 1 -r
+    git status
+    read -p $'\e[31mWould you like to keep the CHANGELOG.md updates?\e[0m: ' -n 1 -r
     echo # newline
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        git restore CHANGELOG.md
         exit 1
     fi
-    git restore CHANGELOG.md
     exit 1
 fi
 branch=$(git branch --show-current)
 [[ $branch != release* ]] && git checkout -b release/$ver
 branch=$(git branch --show-current)
-git add CHANGELOG.md
-git status
-read -p $'\e[31mDouble check the staged files. Would you like to commit these changes?\e[0m: ' -n 1 -r
-echo # newline
-git status
-echo # newline
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    exit 1
-fi
-git commit -m "chore(release): prepare for ${ver}"
+echo "${branch} created!"
 read -p $'\e[31mWould you like to bump the version now?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
-# cdfront
-# npm version $ver
-# cdback
-# npm version $ver
 cd $directory/../
-git add .
 cz bump --files-only
-echo "${branch} created!"
+git status
+read -p $'\e[31mWould you like to stage release changes?\e[0m: ' -n 1 -r
+echo # newline
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    git status
+    read -p $'\e[31mWould you like to undo these updates?\e[0m: ' -n 1 -r
+    echo # newline
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        exit 1
+    fi
+    git restore .
+    exit 1
+fi
+git add .
+git commit -m "chore(release): prepare for ${ver}"
 echo
 read -p $'\e[31mWould you like to push to origin now?\e[0m: ' -n 1 -r
 echo # newline
