44960ad0ed4ceb7cdd762b7ddd9d31ffb7574bfe
Dan Grebb <dan@dgrebb.com>
2023-10-09 20:11:57 -0400

    refactor(ci): reduces cli prompt lengths and adds back bump commit message

--- _utils/_scripts/bump.sh
+++ _utils/_scripts/bump.sh
@@ -5,18 +5,22 @@ cd $directory/../
 config="_utils/_cliff/config.toml"
 file="CHANGELOG.md"
 str=$(cz bump --dry-run | awk '/tag to create: /')
+curr=$(cz version -p)
 ver=${str//tag to create: /}
 git cliff -c $config --unreleased --tag $ver --prepend ${file}
 git ls-files --modified
-read -p $'\e[31mWould you like to cut the release branch now?\e[0m: ' -n 1 -r
+read -p $'\e[31mCut the release branch now?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     git status
-    read -p $'\e[31mWould you like to keep the CHANGELOG.md updates?\e[0m: ' -n 1 -r
+    read -p $'\e[31mKeep the CHANGELOG.md updates?\e[0m: ' -n 1 -r
     echo # newline
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         git restore CHANGELOG.md
         exit 1
+    else
+        git add .
+        git commit -m "chore(release): prepare for ${ver}"
     fi
     exit 1
 else
@@ -25,7 +29,7 @@ else
     branch=$(git branch --show-current)
     echo "${branch} created!"
 fi
-read -p $'\e[31mWould you like to bump the version now?\e[0m: ' -n 1 -r
+read -p $'\e[31mBump the version now?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
@@ -38,11 +42,11 @@ else
     cz bump --files-only
     git status
 fi
-read -p $'\e[31mWould you like to stage release changes?\e[0m: ' -n 1 -r
+read -p $'\e[31mStage file bump changes?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     git status
-    read -p $'\e[31mWould you like to undo these updates?\e[0m: ' -n 1 -r
+    read -p $'\e[31mUndo these updates?\e[0m: ' -n 1 -r
     echo # newline
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit 1
@@ -51,10 +55,10 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 else
     git add .
-    git commit -m "chore(release): prepare for ${ver}"
+    git commit -m "release ${curr} → ${ver}"
 fi
 echo
-read -p $'\e[31mWould you like to push to origin now?\e[0m: ' -n 1 -r
+read -p $'\e[31mPush to origin now?\e[0m: ' -n 1 -r
 echo # newline
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
