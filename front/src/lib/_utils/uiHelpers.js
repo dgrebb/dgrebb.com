7be167b3b0075442addb76dd3af11c8f61fedd1b
Dan Grebb <dan@dgrebb.com>
2023-10-17 10:35:32 -0400

    fix(front): updates uiHelpers for automation querystring scenarios

--- front/src/lib/_utils/uiHelpers.js
+++ front/src/lib/_utils/uiHelpers.js
@@ -1,23 +1,19 @@
 export const tocClick = (heading) => {
-  return window.location.search?.substring(1) === 'roboto'
-    ? false
-    : plausible('TOC Click', { props: { heading } });
+  if (typeof plausible === 'function')
+    plausible('TOC Click', { props: { heading } });
 };
 
 export const categoryClick = (page, category) => {
-  return window.location.search?.substring(1) === 'roboto'
-    ? false
-    : plausible('Category Click', { props: { page, category } });
+  if (typeof plausible === 'function')
+    plausible('Category Click', { props: { page, category } });
 };
 
 export const relatedClick = (page, title) => {
-  return window.location.search?.substring(1) === 'roboto'
-    ? false
-    : plausible('Related Post Click', { props: { page, title } });
+  if (typeof plausible === 'function')
+    plausible('Related Post Click', { props: { page, title } });
 };
 
 export const themeToggleClick = (theme) => {
-  return window.location.search?.substring(1) === 'roboto'
-    ? false
-    : plausible('Theme Toggled', { props: { theme } });
+  if (typeof plausible === 'function')
+    plausible('Theme Toggled', { props: { theme } });
 };
