391b9cf6fb2b63013e4b3c26e4af36bae99d3067
Dan Grebb <dan@dgrebb.com>
2023-10-18 16:29:59 -0400

    chore(front): updates warmup script and adjusts homepage image

--- _ci/_utils/warmup.sh
+++ _ci/_utils/warmup.sh
@@ -38,7 +38,7 @@ warmUp() {
     # If there are particular subdirectories in your sitemap that you do not wish to parse
     # (i.e. because they cannot be cached), you can use the following regex:
     # grep -oP "https?://$1\/((?!subdirectory))[^<]*"
-    wget --user-agent="$USERAGENT" -q "https://$1/sitemap.xml" --output-document - | egrep -o "https?://$1[^<]+" | wget -i -
+    wget ---agent="$USERAGENT"q "https://$1itemap.xml" --output-document - | egrep -o "https?://$1[^<]+" | sed 's/$/\?roboto/' | wget -i -
 }
 
 if [ $# -eq 0 ]; then
