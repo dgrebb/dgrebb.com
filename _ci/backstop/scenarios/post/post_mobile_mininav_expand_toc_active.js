cddd7506e8dfdd9b7aa388044361f252f23910bf
Dan Grebb <dan@dgrebb.com>
2023-10-17 22:08:44 -0400

    test(backstop): fixes mininav active onload test and updates local refs
    gp

--- _ci/backstop/scenarios/post/post_mobile_mininav_expand_toc_active.js
+++ _ci/backstop/scenarios/post/post_mobile_mininav_expand_toc_active.js
@@ -1,11 +1,12 @@
 const { URL, upToMediumViewports } = require('../vars');
-const POST_URL = `${URL}/post/hello-world/#what/?roboto`;
+const POST_URL = `${URL}/post/hello-world/?roboto#what`;
 
 module.exports = {
   label: 'Post - Mobile Mininav TOC Active Onload',
   url: POST_URL,
   viewports: upToMediumViewports,
   onReadyScript: 'playwright/onReadyPosts.js',
+  delay: 333,
   clickSelector: '.page-navigation.mini .page-navigation-toggle',
   postInteractionWait: 1000,
   selectors: ['.page-navigation.mini'],
