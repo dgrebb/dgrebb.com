0be4ff378ec7aadecf3de39ce6b51b06fa63bcf0
Dan Grebb <dan@dgrebb.com>
2023-10-18 17:00:09 -0400

    chore(perf): updates lighthouse configs

--- _ci/perf/lighthouse/.lighthouserc.desktop.cjs
+++ _ci/perf/lighthouse/.lighthouserc.desktop.cjs
@@ -18,6 +18,12 @@ module.exports = {
       deviceScaleFactor: 1,
       disabled: false,
     },
+    skipAudits: [
+      // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
+      "uses-http2",
+      // There are always bf-cache failures when testing in headless. Reenable when headless can give us realistic bf-cache insights.
+      "bf-cache",
+    ],
     emulatedUserAgent:
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
   },
--- _ci/perf/lighthouse/.lighthouserc.mobile.cjs
+++ _ci/perf/lighthouse/.lighthouserc.mobile.cjs
@@ -1,6 +1,13 @@
 module.exports = {
   extends: "lighthouse:default",
   settings: {
-    psiToken: process.env.PSI_APIKEY,
+    maxWaitForFcp: 15 * 1000,
+    maxWaitForLoad: 35 * 1000,
+    skipAudits: [
+      // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
+      "uses-http2",
+      // There are always bf-cache failures when testing in headless. Reenable when headless can give us realistic bf-cache insights.
+      "bf-cache",
+    ],
   },
 };
