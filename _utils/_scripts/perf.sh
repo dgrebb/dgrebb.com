d8d25c3dfc9e42044fb5fbde00edfd3a1c7554fd
Dan Grebb <dan@dgrebb.com>
2023-10-20 18:23:52 -0400

    chore(testing): adjusts how lighthouse applies automation querystring

--- _utils/_scripts/perf.sh
+++ _utils/_scripts/perf.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 source $directory/_scripts/functions.sh
 
-if [ $1 == "s" ]; then
-  URL="https://stg.dgrebb.com?roboto"
-elif [ $1 == "p" ]; then
-  URL="https://www.dgrebb.com?roboto"
+if [[ $1 == "s" ]]; then
+  URL="https://stg.dgrebb.com"
+elif [[ $1 == "p" ]]; then
+  URL="https://www.dgrebb.com"
 else
-  URL="https://local.dgrebb.com?roboto"
+  URL="https://local.dgrebb.com"
 fi
 
-urls="$URL
-$URL/posts
-$URL/privacy"
+urls="${URL}/index.html?roboto
+${URL}/posts/index.html?roboto
+${URL}/post/hello-world/index.html?roboto
+${URL}/posts/category/all/index.html?roboto
+${URL}/privacy/index.html?roboto"
 
+trap exit 0 INT
 cdperf
 while IFS="" read -r url || [ -n "$url" ]; do
-  printf '%s\n' "$url"
-  URL=$url PSI_APIKEY=$(pass dg/keys/page-speed-insights) npm run lh
-  URL=$url PSI_APIKEY=$(pass dg/keys/page-speed-insights) npm run lh-d
+  u="$(echo "$url" | tr -d '\r')"
+  undurl=$(echo "$u" | sed -E 's|https?://([^/]+)(/[^?#]*)?([?#]?.*)?|\2|;s|/+$||;s|/|_|g')
+  echo "Running Lighthouse on $u and writing report to ./${undurl}"
+  npm run lhgh $u -- --output-path ./reports/mobile_${undurl}report.html --disable-extensions --view
+  npm run lhgh-d $u -- --output-path ./reports/desktop_${undurl}report.html --disable-extensions --view
 done <<<"$urls"
