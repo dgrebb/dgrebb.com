474d15c638345be7d92f49141ce41eac1a2d2f39
Dan Grebb <dan@dgrebb.com>
2023-10-22 18:28:51 -0400

    feat(front): adds Code copy event and data (#885)
    Closes: #885

--- front/src/lib/_utils/uiHelpers.js
+++ front/src/lib/_utils/uiHelpers.js
@@ -10,10 +10,32 @@ export const categoryClick = (page, category) => {
 
 export const relatedClick = (page, title) => {
   if (typeof plausible === 'function')
-    plausible('Related Post Click', { props: { page, title } });
+    plausible('Related Post Click', {
+      props: { page, relatedPostTitle: title },
+    });
 };
 
 export const themeToggleClick = (theme) => {
   if (typeof plausible === 'function')
     plausible('Theme Toggled', { props: { theme } });
+};
+
+export const codeCopyClick = (
+  pageTitle,
+  pageSlug,
+  codeTitle = 'Untitled',
+  componentInstanceID
+) => {
+  if (typeof plausible === 'function')
+    plausible('Code Copy', {
+      props: {
+        pageTitle,
+        pageSlug,
+        codeTitle,
+        codeIdentifier: `code_copy__${pageSlug.replace(
+          /-/g,
+          '_'
+        )}__${componentInstanceID}`,
+      },
+    });
 };
