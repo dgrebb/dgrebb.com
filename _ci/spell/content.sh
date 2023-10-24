be846767ee0a70cb46ed7f996b84eb0191e184b5
Dan Grebb <dan@dgrebb.com>
2023-10-23 20:08:10 -0400

    chore(testing): updates ci testing tools with node 20 (#860)

--- _ci/spell/content.sh
+++ _ci/spell/content.sh
@@ -2,7 +2,7 @@
 
 # Checks final build spelling
 
-SITEMAP="${1:-https://stg.dgrebb.com/sitemap.xml}"
+SITEMAP="${1:-https://www.dgrebb.com/sitemap.xml}"
 urls=$(curl -s $SITEMAP | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
 for url in $urls; do
   echo "$url"
