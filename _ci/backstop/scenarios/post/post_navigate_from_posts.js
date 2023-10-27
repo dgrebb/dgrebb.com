c7873c6fe7841b45eeab75674788d63c6f9e0533
Dan Grebb <dan@dgrebb.com>
2023-10-27 02:12:21 -0400

    test(backstop): updates gh-stg refs

--- _ci/backstop/scenarios/post/post_navigate_from_posts.js
+++ _ci/backstop/scenarios/post/post_navigate_from_posts.js
@@ -4,10 +4,11 @@ const POSTS_URL = `${URL}/posts/?roboto`;
 module.exports = {
   label: 'Post - Navigate From Posts',
   url: POSTS_URL,
+  delay: 100,
   onReadyScript: 'playwright/onReadyPost.js',
   clickSelector:
     'a[href*="/post/animating-page-and-view-transitions-with-accessibility-in-mind"]',
-  postInteractionWait: 100,
+  postInteractionWait: 200,
   selectors: ['viewport'],
   selectorExpansion: false,
   misMatchThreshold: 0.2,
