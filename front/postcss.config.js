3ce5edd607207eacb091b86c45b9e08aa1cc5c57
Dan Grebb <dan@dgrebb.com>
2023-11-01 22:22:48 -0400

    feat(front): removes tailwind from project (#622)
    Closes: #622

--- front/postcss.config.js
+++ front/postcss.config.js
@@ -1,13 +1,6 @@
-import tailwind from 'tailwindcss';
-import tailwindConfig from './tailwind.config.js';
 import autoprefixer from 'autoprefixer';
 import atImport from 'postcss-import';
-import tailwindcssNesting from 'tailwindcss/nesting/index.js';
+import postcssNesting from 'postcss-nesting';
 export default {
-  plugins: [
-    atImport,
-    tailwindcssNesting,
-    tailwind(tailwindConfig),
-    autoprefixer,
-  ],
+  plugins: [atImport, postcssNesting, autoprefixer],
 };
