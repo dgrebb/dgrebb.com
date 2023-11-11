a7e078f04558030c1b6397ded4b2451826a52469
Dan Grebb <dan@dgrebb.com>
2023-11-11 17:13:58 -0500

    feat(back): adds landing page singletons and shared fields (#971)
    Closes: #971

---
+++ back/src/api/skills-page/controllers/skills-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skills-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::skills-page.skills-page');
---
+++ back/src/api/skills-page/services/skills-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * skills-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::skills-page.skills-page');
