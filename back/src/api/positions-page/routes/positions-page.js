30723a42cf793f0e492026757547415aa9f0265a
Dan Grebb <dan@dgrebb.com>
2023-11-11 16:02:48 -0500

    feat(back): adds relations and fields for positions and projects (#966, #962)
    Closes: #966 #962

---
+++ back/src/api/positions-page/routes/positions-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * positions-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::positions-page.positions-page');
