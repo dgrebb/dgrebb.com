7608cfd040c0884d1535d709ceff73b8b906d269
Dan Grebb <dan@dgrebb.com>
2023-11-11 22:06:18 -0500

    build(front): refactors active navigation system

--- front/src/lib/layout/NavBar.svelte
+++ front/src/lib/layout/NavBar.svelte
@@ -2,21 +2,17 @@
   import { page } from '$app/stores';
   // import NavToggle from "../components/NavToggle.svelte";
   export let navItems;
-  $: postsActive = $page?.route?.id?.includes('post') || false;
-  $: subactive =
-    $page?.route?.id === '/post/[slug]' ||
-    $page?.route?.id === '/posts/category/[category]' ||
-    false;
+  $: route = $page?.route?.id;
 </script>
 
 <nav class="nav-bar" aria-label="Site Pages">
   <!-- <NavToggle /> -->
   <ul class="nav-list">
-    {#each navItems as { href, title }}
+    {#each navItems as { href, title, childRoutes }}
+      {@const active = href === route || childRoutes.includes(route) || false}
+      {@const subactive = childRoutes.includes(route) || false}
       <li class="nav-item">
-        <a class="nav-link" class:active={postsActive} class:subactive {href}
-          >{title}</a
-        >
+        <a class="nav-link" class:active class:subactive {href}>{title}</a>
       </li>
     {/each}
   </ul>
