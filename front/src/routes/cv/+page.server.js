244dafe91b055d075073719ecdbe47e92a1a21f8
Dan Grebb <dan@dgrebb.com>
2023-11-11 22:35:18 -0500

    feat(front): creates CV api, server, and page routes (#832)
    Closes: #832

---
+++ front/src/routes/cv/+page.server.js
@@ -0,0 +1,52 @@
+import {
+  PUBLIC_CV_PAGE as CV,
+  PUBLIC_API_URL as URL,
+} from '$env/static/public';
+import { cvAPI } from '@api';
+import { error } from '@sveltejs/kit';
+
+const endpoint = URL + CV;
+
+export async function load({ params: { pathname } }) {
+  var cv;
+  let seo, hero, title, intro;
+
+  try {
+    cv = await cvAPI(endpoint);
+  } catch (error) {
+    console.warn('CV page API error.');
+    console.error(error);
+  }
+
+  if (!cv) {
+    throw error(500, 'CV Page Error');
+  }
+
+  ({
+    seo,
+    content: { hero, title, introduction: intro },
+  } = cv);
+
+  const page = {
+    title,
+    intro,
+    hero: hero.data.attributes,
+  };
+
+  var pageMeta = {
+    ...seo,
+    type: 'website',
+    metaTitle: seo?.metaTitle || title,
+  };
+
+  /**
+   * Isolates the `metaImage` object properties we care about
+   */
+  pageMeta.metaImage =
+    pageMeta?.metaImage?.data?.attributes || hero?.data?.attributes || false;
+
+  return {
+    page,
+    pageMeta,
+  };
+}
---
+++ front/src/routes/cv/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Meta from '@components/Meta.svelte';
+  import PageTransition from '@components/PageTransition.svelte';
+  import Flourish from '@layout/Flourish.svelte';
+
+  export let data;
+  let positionsContent;
+  let projectsContent;
+  let skillsContent;
+  let organizationsContent;
+
+  const { pathname, page, pageMeta } = data;
+  const { title, hero, intro } = page;
+</script>
+
+<PageTransition transitionKey={pathname}>
+  <section class="cv">
+    <Flourish />
+    <a id="main">Main Content</a>
+    <h1>{title}</h1>
+    <img src={hero.url} alt={hero.alternativeText} />
+    <div class="summary">
+      {intro}
+    </div>
+  </section>
+</PageTransition>
+<Meta {pageMeta} />
