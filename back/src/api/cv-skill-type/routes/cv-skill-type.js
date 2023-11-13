c1bf22f242b67c2eeeeb99b1fdd1714fdba30c11
Dan Grebb <dan@dgrebb.com>
2023-11-12 19:44:15 -0500

    feat(back): adds skill type collection, fields, and schema (#976)

---
+++ back/src/api/cv-skill-type/routes/cv-skill-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cv-skill-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cv-skill-type.cv-skill-type');
---
+++ back/src/api/cv-skill-type/services/cv-skill-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cv-skill-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cv-skill-type.cv-skill-type');
