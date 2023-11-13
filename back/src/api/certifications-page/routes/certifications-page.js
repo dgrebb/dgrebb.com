196e5a1eff3f051f6c75470f32c368ac161993c4
Dan Grebb <dan@dgrebb.com>
2023-11-13 14:50:54 -0500

    build(back): adds certifications collection, fields, schema, and relationships (#984)
    Closes: #984

---
+++ back/src/api/certifications-page/routes/certifications-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certifications-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::certifications-page.certifications-page');
---
+++ back/src/api/certifications-page/services/certifications-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certifications-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::certifications-page.certifications-page');
