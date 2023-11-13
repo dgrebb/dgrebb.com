ef4577e2e4387f96a6b01cb4c5b94b80b7b9e00b
Dan Grebb <dan@dgrebb.com>
2023-11-12 19:44:15 -0500

    feat(back): adds skill type collection, fields, and schema (#976)

---
+++ back/src/api/cv-skill-type/controllers/cv-skill-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cv-skill-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cv-skill-type.cv-skill-type');
