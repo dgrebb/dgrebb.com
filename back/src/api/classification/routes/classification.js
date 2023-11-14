665c48dc2521dab182d8d1cd2c29a4e6de9225af
Dan Grebb <dan@dgrebb.com>
2023-11-13 21:59:39 -0500

    refactor(back): refactor classifications collection, naming, and relationships (#965)

---
+++ back/src/api/classification/routes/classification.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * classification router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::classification.classification');
---
+++ back/src/api/classifications-page/routes/classifications-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * classifications-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::classifications-page.classifications-page');
