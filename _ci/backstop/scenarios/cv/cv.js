4c5ff47ce0a46dff98845134079dbce76dc1472b
Dan Grebb <dan@dgrebb.com>
2023-11-23 23:59:21 -0500

    test(backstop): updates playwright, moves scripts, and sets up CV test pattern (#980)

--- _ci/backstop/scenarios/cv/cv.js
+++ _ci/backstop/scenarios/cv/cv.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 fs.readdirSync(__dirname).forEach(function(file) {
-  if (file.indexOf(".js") > -1 && file != "cats.js") {
+  if (file.indexOf(".js") > -1 && file != "cv.js") {
     exports[file.replace(".js", "")] = require("./" + file);
   }
 });
---
+++ _ci/backstop/scripts/playwright/overridePostCSS.js
@@ -0,0 +1,27 @@
+const BACKSTOP_TEST_CSS_OVERRIDE = `
+  * {
+    transition: none !important; 
+    transition-duration: 0 !important; 
+    transition-delay: 0 !important;
+  }
+  html {
+    height: 100% !important;
+  }
+  body {
+    overflow: visible !important;
+    margin-right: 1rem;
+    position: absolute !important;
+    top: 0 !important;
+    bottom: 0 !important;
+    left: 0 !important;
+    right: 0 !important;
+  }
+`;
+
+module.exports = async (page, scenario) => {
+  await page.addStyleTag({
+    content: BACKSTOP_TEST_CSS_OVERRIDE,
+  });
+
+  console.log('BACKSTOP_TEST_CSS_OVERRIDE injected for: ' + scenario.label);
+};
