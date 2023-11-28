e1b5a5e41f2906bb8293f2c5dd0669921393bb2c
Dan Grebb <dan@dgrebb.com>
2023-11-28 15:45:47 -0500

    WIP(back): adds experience collection-type, fields, and configuration (#1027)

---
+++ back/src/api/experiences-page/services/experiences-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * experiences-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::experiences-page.experiences-page');
