62f17d76875fae7ef418df80af841d16d83dc660
Dan Grebb <dan@dgrebb.com>
2023-11-28 15:45:47 -0500

    WIP(back): adds experience collection-type, fields, and configuration (#1027)

---
+++ back/src/api/experiences-page/controllers/experiences-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * experiences-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::experiences-page.experiences-page');
