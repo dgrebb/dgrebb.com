1a47b882bd632853277d7c173c4c4a5f4fce6eb4
Dan Grebb <dan@dgrebb.com>
2023-12-03 00:50:49 -0500

    chore(back): removes positions content-type and skills icon and svg fields (#1044)

--- back/src/api/positions-page/controllers/positions-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * positions-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::positions-page.positions-page');
--- back/src/api/positions-page/services/positions-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * positions-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::positions-page.positions-page');
