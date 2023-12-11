3158e0e4ef02aafa5d4599fe05877126533a6c5a
Dan Grebb <dan@dgrebb.com>
2023-12-10 21:23:50 -0500

    perf(front): improves parser and renderer performance (#1061)

--- front/src/lib/components/content/parsers/index.js
+++ front/src/lib/components/content/parsers/index.js
@@ -1,13 +1,24 @@
 import { marked } from 'marked';
 import slugger from 'slugger';
 
+/**
+ * Parses the table of contents from markdown content.
+ *
+ * @param {string} contents - The markdown content.
+ * @returns {Array} - Array of objects representing headings.
+ */
 export const parseTOC = function (contents) {
   const tokens = marked.lexer(contents);
-  const headings = tokens.filter((t) => t.type === 'heading' && t.depth <= 2);
-  return [
-    ...headings.map((h) => ({
-      text: marked.parseInline(h.text),
-      link: `#${slugger(h.text)}`,
-    })),
-  ];
+  const headings = [];
+
+  tokens.forEach((t) => {
+    if (t.type === 'heading' && t.depth <= 2) {
+      headings.push({
+        text: marked.parseInline(t.text),
+        link: `#${slugger(t.text)}`,
+      });
+    }
+  });
+
+  return headings;
 };
