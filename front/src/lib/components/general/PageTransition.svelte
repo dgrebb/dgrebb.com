5604b20c9450a448824a36b831b908f87bc5e084
Dan Grebb <dan@dgrebb.com>
2023-12-11 20:29:21 -0500

    perf(front): improves performance of home and general components (#1061)

--- front/src/lib/components/general/PageTransition.svelte
+++ front/src/lib/components/general/PageTransition.svelte
@@ -5,39 +5,64 @@
   export let transitionKey;
   let to;
 
+  /**
+   * Calculates the animation duration based on motionless condition.
+   * @function
+   * @returns {Object} Object with a duration property.
+   */
   function doIt() {
+    const isMotionless = motionless();
     return {
-      duration: motionless() === true ? 0 : 300,
+      duration: isMotionless ? 0 : 333,
     };
   }
 
+  /**
+   * Initiates the outro animation.
+   * @async
+   * @function
+   * @returns {Promise<void>} A promise that resolves after the animation starts.
+   */
   async function animateOutroStart() {
-    if (motionless() === true) return;
+    if (motionless()) return;
     const header = document.querySelector('.header');
+    const isHeaderOutsideViewport = isElementOutsideViewport(header);
     to = $navigating?.to.route.id;
     document.body.classList.toggle('animating', true);
-    if (isElementOutsideViewport(header)) {
+    if (isHeaderOutsideViewport) {
       header.classList.toggle('scroll-transition', true);
     }
   }
 
+  /**
+   * Finalizes the outro animation.
+   * @function
+   */
   function animateOutroEnd() {
     if (to === '/') return;
     scrollTop();
   }
-
+  /**
+   * Initiates the intro animation.
+   * @function
+   */
   function animateIntroStart() {
-    if (motionless() === true) return;
+    if (motionless()) return;
   }
 
+  /**
+   * Finalizes the intro animation using requestAnimationFrame for better performance.
+   * @function
+   */
   function animateIntroEnd() {
-    if (motionless() === true) return;
+    if (motionless()) return;
+
     const header = document.querySelector('.header');
-    setTimeout(() => {
-      header.classList.toggle('scroll-transition', false);
-      document.body.classList.toggle('animating', false);
-      document.body.classList.toggle('animating-page', false);
-    }, 333);
+
+    requestAnimationFrame(() => {
+      header.classList.remove('scroll-transition');
+      document.body.classList.remove('animating', 'animating-page');
+    });
   }
 </script>
 
--- front/src/lib/components/general/TransitionFade.svelte
+++ front/src/lib/components/general/TransitionFade.svelte
@@ -1,21 +1,62 @@
 <script>
+  /**
+   * @script
+   * This script controls a transition effect for a fade container in a SvelteKit application.
+   * @module FadeTransition
+   */
+
   import { motionless } from '@utils';
+
+  /**
+   * Key for the transition.
+   * @type {string}
+   */
   export let transitionKey;
-  export let duration = 500;
+
+  /**
+   * Duration of the transition.
+   * @type {number}
+   * @default 333
+   */
+  export let duration = 333;
+
+  /**
+   * Delay before the transition starts.
+   * @type {number}
+   * @default 600
+   */
   export let delay = 600;
+
+  /**
+   * Optional class list for the fade container.
+   * @type {string|false}
+   * @default false
+   */
   export let classList = false;
 
+  /**
+   * Determines the transition duration based on the `motionless()` function.
+   * @returns {Object} - Object with the transition duration.
+   */
   function doIt() {
     return {
       duration: motionless() === true ? 0 : duration,
     };
   }
 
+  /**
+   * Initiates the fade-out animation.
+   * @returns {boolean} - False if motionless, true otherwise.
+   */
   function animateOut() {
     if (motionless() === true) return false;
     document.body.classList.toggle('animating-page', true);
   }
 
+  /**
+   * Initiates the fade-in animation after a delay.
+   * @returns {boolean} - False if motionless, true otherwise.
+   */
   function animateIn() {
     if (motionless() === true) return false;
     setTimeout(() => {
@@ -25,6 +66,7 @@
   }
 </script>
 
+/** * Component rendering the fade container with transition effects. */
 {#key transitionKey}
   <div
     class="transition-fade-container {classList ? classList : ''}"
