1d267d51713d9e4272c399e516e75d4d74822d11
Dan Grebb <dan@dgrebb.com>
2023-12-14 20:29:27 -0500

    wip(front): removes vitest from local vite config

--- front/vite.local.config.js
+++ front/vite.local.config.js
@@ -11,7 +11,6 @@ import postcss from './postcss.config.js';
 
 export default defineConfig({
   plugins: [
-    svelte({ hot: !process.env.VITEST }),
     sveltekit(),
     Icons({
       compiler: 'svelte',
