f2b8497c697bd9f66dc28e0490518146f23cdeb8
Dan Grebb <dan@dgrebb.com>
2023-12-16 04:22:24 -0500

    feat(front): renders title else domain for external markdown link titles (#1050)

--- front/src/lib/_utils/index.js
+++ front/src/lib/_utils/index.js
@@ -22,7 +22,7 @@ export const themeName = async function (preference) {
   return preference ? darkTheme : lightTheme;
 };
 
-export const scrollTop = (e) => {
+export const scrollTop = function () {
   e?.preventDefault();
   document.getElementById('header')?.scrollIntoView();
 };
@@ -46,6 +46,24 @@ export const isElementOutsideViewport = (element) => {
  * Determine if a browser/system setting prefers reduced motion
  * @returns boolean
  */
-export const motionless = () => {
+export const motionless = function () {
   return window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+};
+
+/**
+ * Extracts the domain from a URL without the 'www' prefix.
+ *
+ * @param {string} url - The URL from which to extract the domain.
+ * @returns {string|null} The extracted domain without 'www' or null if no match is found.
+ */
+export const extractDomainWithoutWWW = function (url) {
+  const domainRegex = /^(?:https?:\/\/)?(?:www\.)?([^/]+)/;
+
+  const match = url.match(domainRegex);
+
+  if (match && match[1]) {
+    return match[1];
+  } else {
+    return null;
+  }
 };
