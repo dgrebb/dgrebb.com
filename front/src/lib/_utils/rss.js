9f26830f2830fed74f5cc281f559d257d9275372
Dan Grebb <dan@dgrebb.com>
2023-12-24 19:01:47 -0500

    feat(front): adds RSS route, server, and xml (#1108)
    Closes: #1108

---
+++ front/src/lib/_utils/rss.js
@@ -0,0 +1,50 @@
+const website = 'https://www.dgrebb.com';
+const feedTitle = 'Dan Grebb | Computer Programmer';
+const feedDescription = 'Thoughts, learnings, and updates from Dan Grebb.';
+const feedLink = 'https://www.dgrebb.com';
+const feedUpdated = new Date();
+
+export const xml = (posts) => `<?xml version="1.0" encoding="utf-8"?>
+  <feed xmlns="http://www.w3.org/2005/Atom">
+    <title>${feedTitle}</title>
+    <link href="${feedLink}/rss.xml" rel="self"/>
+    <link href="${feedLink}"/>
+    <id>${feedLink}/</id>
+    <updated>${feedUpdated.toISOString()}</updated>
+    <author>
+      <name>Dan Grebb</name>
+    </author>
+    <subtitle>${feedDescription}</subtitle>
+    <generator>JavaScript</generator>
+${posts
+  .map((post) => {
+    const { slug, publishedAt, summary } = post.attributes;
+
+    const image =
+      post?.attributes?.hero?.data?.attributes?.formats?.thumbnail?.url ||
+      false;
+
+    const { altText } = post?.attributes?.hero?.data?.attributes || false;
+
+    return `<entry>
+        <title>${post.attributes.title}</title>
+        <link href="${website}/post/${slug}/"/>
+        <id>${website}/post/${slug}/</id>
+        <updated>${new Date(publishedAt).toISOString()}</updated>
+        <published>${new Date(publishedAt).toISOString()}</published>
+        ${
+          summary ? `<content type="html"><![CDATA[${summary}]]></content>` : ''
+        }
+        ${
+          image
+            ? `<image>
+                <url>${image}</url>
+                <title>${altText}</title>
+                <link>${slug}</link>
+              </image>`
+            : ''
+        }
+      </entry>`;
+  })
+  .join('\n')}
+  </feed>`;
