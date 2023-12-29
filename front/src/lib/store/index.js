7fd2242802494119708d8db17920a512d52a7198
Dan Grebb <dan@dgrebb.com>
2023-12-29 01:03:36 -0500

    feat(front): adds popover component, api, and styles (#1092)
    Closes #1092

--- front/src/lib/store/index.js
+++ front/src/lib/store/index.js
@@ -1,3 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const pageMeta = writable({ titleTemplate: false });
+export const popover = writable({
+  show: false,
+  title: 'Whoops!',
+  content: `<p>...there's nothing here.</p>`,
+});
