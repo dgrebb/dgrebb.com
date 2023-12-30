bbc17b5e65faaa3eebf0e8126dd37ccb396ca1be
Dan Grebb <dan@dgrebb.com>
2023-12-30 00:50:30 -0500

    test(backstop): updates popover style and adds test and refs (#1119)
    Closes: #1119

--- front/src/lib/_hooks/hooks.client.js
+++
@@ -1,8 +0,0 @@
-// export async function handleError({ error, event }) {
-//   setTag('environment', PUBLIC_ENV);
-//   captureException(error, { extra: { event } });
-
-//   return {
-//     message: 'Client error.',
-//   };
-// }
