32b8a1f99fb377cdd9f010b49da6819a3f3f104d
Dan Grebb <git@dgrebb.com>
2024-04-16 18:46:24 -0400

    chore(terraform): upgrade to terraform 1.8.0 and aws provider 5.45.0

--- _tf/prd/provider.tf
+++ _tf/prd/provider.tf
@@ -8,10 +8,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.33.0"
+      version = "~> 5.45.0"
     }
   }
   backend "s3" {}
 
-  required_version = "~> 1.7.0"
+  required_version = "~> 1.8.0"
 }
